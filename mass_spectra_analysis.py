cbbf8f4ecff5218859b767449641691c307a5959
Alexander Shlemov <shlemovalex@gmail.com>
2015-07-07 01:08:59 +0300

    More verbosity

--- mass_spectra_analysis.py
+++ mass_spectra_analysis.py
@@ -354,3 +354,4 @@ if __name__ == '__main__':
     metrics.CalculateMetrics()
     metrics.CalculateRegionMetrics()
     metrics.OutputMetrics(args.output_dir)
+    print "Figures and statistics saved in %s" % args.output_dir
