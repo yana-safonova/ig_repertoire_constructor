a9267bae38875ee3b567d609edae211f7918b01b
Alexander Shlemov <shlemovalex@gmail.com>
2015-08-25 02:07:41 +0300

    toSquash

--- src/ig_quast_tool/rcm_cmp.py
+++ src/ig_quast_tool/rcm_cmp.py
@@ -7,6 +7,35 @@ from barcode_cleaner import smart_open
 from ig_basic import extract_barcode, most_popular_element
 from collections import defaultdict
 import numpy as np
+from Levenshtein import distance as levenshtein
+
+
+def consensus(reads):
+    import numpy as np
+
+    n = min(map(len, reads))
+
+    reads = reads[:]
+    reads = [read for read in reads if len(read) > 0]
+    if len(reads) == 0 or n == 0:
+        return None
+
+    for i in xrange(len(reads)):
+        reads[i] = reads[i][:n]
+
+    nucleotides = np.array(['A', 'C', 'G', 'T'])
+    nuc_ind = {nucleotides[i]: i for i in range(len(nucleotides))}
+
+    consm = np.zeros((4, n), dtype=int)
+    for j in xrange(len(reads)):
+        s = reads[j]
+        for i in xrange(n):
+            consm[nuc_ind[s[i]], i] += 1
+
+    consensus = "".join(nucleotides[np.argmax(consm, axis=0)])
+
+    return consensus
+
 
 if __name__ == "__main__":
     parser = ArgumentParser(description="Compare rcm-file")
@@ -61,7 +90,26 @@ if __name__ == "__main__":
 
     print "Reading input reads..."
     with smart_open(args.reads, "r") as fh:
-        id2read = {str(record.id): record for record in seqIO.parse(fh, "fasta")}
+        id2read = {str(record.id): str(record.seq)
+                   for record in SeqIO.parse(fh, "fasta")}
+
+    barcode2ids = defaultdict(list)
+    for id in id2clique.keys():
+        barcode = extract_barcode(id)
+        barcode2ids[barcode].append(id)
+
+    barcode2consensus = {}
+    barcode2dists = {}
+    for barcode, ids in barcode2ids.iteritems():
+        cons = barcode2consensus[barcode] = consensus([id2read[id] for id in ids])
+        dists = [levenshtein(cons, id2read[id]) for id in ids]
+        barcode2dists[barcode] = dists
+
+    for barcode in good_barcodes:
+        print barcode2dists[barcode]
+
+
+
 
 
 
