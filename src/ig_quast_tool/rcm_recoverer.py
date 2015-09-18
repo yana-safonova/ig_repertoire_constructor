48e270d4160157d362de0c463dd46c6ed3cda7f3
Alexander Shlemov <shlemovalex@gmail.com>
2015-09-18 23:52:23 +0300

    Get rid of extra dependences in RCM reconstructor

--- src/ig_quast_tool/rcm_recoverer.py
+++ src/ig_quast_tool/rcm_recoverer.py
@@ -2,7 +2,39 @@
 
 from Bio import SeqIO
 from argparse import ArgumentParser
-from barcode_cleaner import smart_open
+
+
+import contextlib
+@contextlib.contextmanager
+def smart_open(filename, mode="r"):
+    """
+    From http://stackoverflow.com/questions/17602878/how-to-handle-both-with-open-and-sys-stdout-nicely
+    """
+    import gzip
+    import re
+    from sys import stdout, stdin
+
+    if "w" in mode:
+        MODE = "w"
+    elif "a" in mode:
+        MODE= "a"
+    else:
+        MODE = "r"
+
+    if filename != '-':
+        if re.match(r"^.*\.gz$", filename):
+            assert(MODE != "a")
+            fh = gzip.open(filename, mode=MODE)
+        else:
+            fh = open(filename, mode=mode)
+    else:
+        assert(MODE != "a")
+        fh = stdout if MODE == "w" else stdin
+    try:
+        yield fh
+    finally:
+        if fh is not stdout and fh is not stdin:
+            fh.close()
 
 
 def generate_rcm(reads_file_name, compressed_file_name, cliques_ids_file_name,
@@ -24,7 +56,7 @@ def generate_rcm(reads_file_name, compressed_file_name, cliques_ids_file_name,
 
 
 if __name__ == "__main__":
-    parser = ArgumentParser(description="Recover rcm-file")
+    parser = ArgumentParser(description="Recover .RCM file from .map file")
     parser.add_argument("--ids-file", "-i",
                         type=str,
                         required=True,
