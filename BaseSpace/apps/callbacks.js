0c2bcfffbb3f7edcf404d981783f7cd48e76374f
Alexander Shlemov <shlemovalex@gmail.com>
2015-10-17 16:19:11 +0300

    Update callback script

--- BaseSpace/apps/callbacks.js
+++ BaseSpace/apps/callbacks.js
@@ -2,7 +2,7 @@ function launchSpec(dataProvider)
 {
   var retval = {
     commandLine: ["python", "/ig_repertoire_constructor/BaseSpace/apps/runIGRC.py"],
-    containerImageId : "eodus/mydock",
+    containerImageId : "docker.illumina.com/ig_repertoire_constructor/igrc:latest",
     Options: [ "bsfs.enabled=true" ]
   };
 
