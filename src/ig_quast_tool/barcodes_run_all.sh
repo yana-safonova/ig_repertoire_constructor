09e078a689a72a4628ab5d36272e324fb999a005
Alexander Shlemov <shlemovalex@gmail.com>
2015-10-26 20:30:19 +0300

    fx

--- src/ig_quast_tool/barcodes_run_all.sh
+++ src/ig_quast_tool/barcodes_run_all.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 DIR="/johnny/data/input/Ig/ibh_datasets/age_datasets/raw_reads/merged_reads/gzipped"
-OUT_DIR = "/home/ashlemov/barcodes_agressive/"
+OUT_DIR="/home/ashlemov/barcodes_agressive/"
 
 mkdir ${OUT_DIR}
 
 for i in `seq 1 9`; do
     ./barcode_cleaner_nojoin.py -m 5 -b ${OUT_DIR}/age_${i}_bad.fq.gz \
-        --rcm $barcodes_${i}.rcm \
+        --rcm ${OUT_DIR}/barcodes_${i}.rcm \
         $DIR/age_ig_s${i}_R12_new.fastq.gz \
         ${OUT_DIR}/age_${i}_good.fq.gz &
 done
 
-join
+wait
