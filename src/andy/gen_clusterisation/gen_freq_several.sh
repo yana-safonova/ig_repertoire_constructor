97631e62b397fbba00bf66d0768594c853332eec
seryrzu <seryrzu@gmail.com>
2015-11-13 16:34:33 +0300

    uncommited changes to talk_12_X

--- src/andy/gen_clusterisation/gen_freq_several.sh
+++ src/andy/gen_clusterisation/gen_freq_several.sh
@@ -1,16 +1,21 @@
-#!/bin/bash
+#./gen_freq.py -s /home/ashlemov/Yandex.Disk/Documents/lab/vers2/test_dataset/age_sam/age/1/final_repertoire_large.fa -a /home/ashlemov/Yandex.Disk/Documents/lab/vers2/test_dataset/age_sam/age_ig_s1.igblast.align -o ./1   
+#./gen_freq.py -s /home/ashlemov/Yandex.Disk/Documents/lab/vers2/test_dataset/age_sam/age/2/final_repertoire_large.fa -a /home/ashlemov/Yandex.Disk/Documents/lab/vers2/test_dataset/age_sam/age_ig_s2.igblast.align -o ./2  
+#./gen_freq.py -s /home/ashlemov/Yandex.Disk/Documents/lab/vers2/test_dataset/age_sam/age/3/final_repertoire_large.fa -a /home/ashlemov/Yandex.Disk/Documents/lab/vers2/test_dataset/age_sam/age_ig_s3.igblast.align -o ./3  
+#./gen_freq.py -s /home/ashlemov/Yandex.Disk/Documents/lab/vers2/test_dataset/age_sam/age/4/final_repertoire_large.fa -a /home/ashlemov/Yandex.Disk/Documents/lab/vers2/test_dataset/age_sam/age_ig_s4.igblast.align -o ./4  
+#./gen_freq.py -s /home/ashlemov/Yandex.Disk/Documents/lab/vers2/test_dataset/age_sam/age/5/final_repertoire_large.fa -a /home/ashlemov/Yandex.Disk/Documents/lab/vers2/test_dataset/age_sam/age_ig_s5.igblast.align -o ./5  
+#./gen_freq.py -s /home/ashlemov/Yandex.Disk/Documents/lab/vers2/test_dataset/age_sam/age/6/final_repertoire_large.fa -a /home/ashlemov/Yandex.Disk/Documents/lab/vers2/test_dataset/age_sam/age_ig_s6.igblast.align -o ./6  
+#./gen_freq.py -s /home/ashlemov/Yandex.Disk/Documents/lab/vers2/test_dataset/age_sam/age/7/final_repertoire_large.fa -a /home/ashlemov/Yandex.Disk/Documents/lab/vers2/test_dataset/age_sam/age_ig_s7.igblast.align -o ./7  
+#./gen_freq.py -s /home/ashlemov/Yandex.Disk/Documents/lab/vers2/test_dataset/age_sam/age/8/final_repertoire_large.fa -a /home/ashlemov/Yandex.Disk/Documents/lab/vers2/test_dataset/age_sam/age_ig_s8.igblast.align -o ./8  
+#./gen_freq.py -s /home/ashlemov/Yandex.Disk/Documents/lab/vers2/test_dataset/age_sam/age/9/final_repertoire_large.fa -a /home/ashlemov/Yandex.Disk/Documents/lab/vers2/test_dataset/age_sam/age_ig_s9.igblast.align -o ./9  
+#
+#./gen_freq.py -s /home/ashlemov/Yandex.Disk/Documents/lab/vers2/test_dataset/age_sam/sam/1_SAM13306969/final_repertoire_stripped.fa -a /home/ashlemov/Yandex.Disk/Documents/lab/vers2/test_dataset/age_sam/1_SAM13306969.igblast.align -o ./1_SAM13306969 
+#./gen_freq.py -s /home/ashlemov/Yandex.Disk/Documents/lab/vers2/test_dataset/age_sam/sam/2_SAM13306970/final_repertoire_stripped.fa -a /home/ashlemov/Yandex.Disk/Documents/lab/vers2/test_dataset/age_sam/2_SAM13306970.igblast.align -o ./2_SAM13306970 
+#./gen_freq.py -s /home/ashlemov/Yandex.Disk/Documents/lab/vers2/test_dataset/age_sam/sam/7_SAM15574987/final_repertoire_large.fa -a /home/ashlemov/Yandex.Disk/Documents/lab/vers2/test_dataset/age_sam/7_SAM15574987.igblast.align -o ./7_SAM15574987 
+#./gen_freq.py -s /home/ashlemov/Yandex.Disk/Documents/lab/vers2/test_dataset/age_sam/sam/8_SAM15574988/final_repertoire_large.fa -a /home/ashlemov/Yandex.Disk/Documents/lab/vers2/test_dataset/age_sam/8_SAM15574988.igblast.align -o ./8_SAM15574988 
 
-INPUT_DIR=$1
-OUTPUT_DIR=$2
 
-GEN_FREQ_DIR='/home/ashlemov/Yandex.Disk/Documents/lab/vers2/src/andy/gen_clusterisation'
+#./gen_freq.py -s /home/ashlemov/Yandex.Disk/Documents/lab/vers2/test_dataset/SAMEA/2/final_repertoire_large.fa -a /home/ashlemov/Yandex.Disk/Documents/lab/vers2/test_dataset/SAMEA/SAMEA2240927_ERX319432.igblast.align -o ./SAMEA2240927_ERX319432
+#./gen_freq.py -s /home/ashlemov/Yandex.Disk/Documents/lab/vers2/test_dataset/SAMEA/6/final_repertoire_large.fa -a /home/ashlemov/Yandex.Disk/Documents/lab/vers2/test_dataset/SAMEA/SAMEA2240927_ERX319436.igblast.align -o ./SAMEA2240927_ERX319436
+#./gen_freq.py -s /home/ashlemov/Yandex.Disk/Documents/lab/vers2/test_dataset/SAMEA/7/final_repertoire_large.fa -a /home/ashlemov/Yandex.Disk/Documents/lab/vers2/test_dataset/SAMEA/SAMEA2240927_ERX319437.igblast.align -o ./SAMEA2240927_ERX319437
 
-
-for file in ${INPUT_DIR}/age_ig_s?_R12_assembled.cleaned.fastq; do
-  echo ${file}
-  filename=$(basename "$file")
-  i="${filename:8:1}"
-  echo $i
-  ${GEN_FREQ_DIR}/gen_freq.py -s ${file} -a "${INPUT_DIR}/igblast_output/age_ig_s${i}_igblast_cleaned.align" -o ${OUTPUT_DIR}/${i} 
-done
-wait
+./gen_freq.py -s /home/ashlemov/Yandex.Disk/Documents/lab/vers2/test_dataset/BSSE_QGF_20433_naive/final_repertoire.fa -a /home/ashlemov/Yandex.Disk/Documents/lab/vers2/test_dataset/BSSE_QGF_20433_naive/BSSE_QGF_20433_naive.igblast.align -o ./BSSE_QGF_20433_naive
