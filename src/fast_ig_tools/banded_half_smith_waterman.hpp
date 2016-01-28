8d08a4ce59ecbfbc4f865f7732c1d7bcb0972e48
Sergey Bankevich <Sergey.Bankevich@gmail.com>
2016-01-28 22:16:33 +0300

    fix type issue

--- src/fast_ig_tools/banded_half_smith_waterman.hpp
+++ src/fast_ig_tools/banded_half_smith_waterman.hpp
@@ -14,15 +14,15 @@ template<typename Ts1, typename Ts2, typename Tf>
 int half_hamming(const Ts1 &s1, const Ts2 &s2,
                  int match, int mismatch,
                  const Tf &lizard_tail) {
-    int len1 = length(s1), len2 = length(s2); // Cache lengths because of length(s) computation maybe not O(1) e.g. for char*-strings
-    size_t len = std::min<size_t>(len1, len2);
+    auto len1 = length(s1), len2 = length(s2); // Cache lengths because of length(s) computation maybe not O(1) e.g. for char*-strings
+    auto len = std::min(len1, len2);
 
-    size_t res = 0;
-    for (size_t i = 0; i < len; ++i) {
+    int res = 0;
+    for (auto i = 0; i < len; ++i) {
         res += (s1[i] == s2[i]) ? match : mismatch;
     }
 
-    res += lizard_tail(std::abs(len1 - len2));
+    res += lizard_tail(std::abs(static_cast<int>(len1) - static_cast<int>(len2)));
 
     return res;
 }
@@ -45,22 +45,22 @@ int half_sw_banded(const Ts1 &s1, const Ts2 &s2,
         return half_hamming(s1, s2, match, mismatch, lizard_tail);
     }
 
-    int len1 = length(s1), len2 = length(s2); // Cache lengths because of length(s) computation maybe not O(1) e.g. for char*-strings
+    auto len1 = length(s1), len2 = length(s2); // Cache lengths because of length(s) computation maybe not O(1) e.g. for char*-strings
 
     vector<int> base(2*max_indels + 1, -INF);
-    for (int i2 = max(0, len1 - max_indels); i2 <= min(len1 + max_indels, len2); ++i2) {
-        int ind = i2 + max_indels - len1;
-        base[ind] = lizard_tail(len2 - i2);
+    for (auto i2 = max(0ul, len1 - max_indels); i2 <= min(len1 + max_indels, len2); ++i2) {
+        auto ind = i2 + max_indels - len1;
+        base[ind] = lizard_tail(static_cast<int>(len2 - i2));
     }
 
     vector<int> new_base(2*max_indels + 1, -INF);
-    for (int i1 = len1 - 1; i1 >= 0; --i1) {
-        for (int i2 = i1 + max_indels; i2 >= i1 - max_indels; --i2) {
-            int inx = max_indels + i2 - i1;
+    for (auto i1 = len1 - 1; i1 >= 0; --i1) {
+        for (auto i2 = i1 + max_indels; i2 >= i1 - max_indels; --i2) {
+            auto inx = max_indels + i2 - i1;
             if ((i2 < 0) || (i2 > len2)) {
                 new_base[inx] = -INF;
             } else if (i2 == len2) {
-                new_base[inx] = lizard_tail(len1 - i1);
+                new_base[inx] = lizard_tail(static_cast<int>(len1 - i1));
             } else {
                 int m = (s1[i1] == s2[i2]) ? match : mismatch;
 
