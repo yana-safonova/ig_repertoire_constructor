a49d68421673475ba30f2901cd23dd6aba6ff52c
Sergey Bankevich <Sergey.Bankevich@gmail.com>
2016-02-10 21:25:23 +0300

    manual read -> seqan for umi extraction

--- src/umi_experiments/umi_to_fastq.cpp
+++ src/umi_experiments/umi_to_fastq.cpp
@@ -3,49 +3,62 @@
 #include <iostream>
 #include <fstream>
 #include <string.h>
+#include <logger/log_writers.hpp>
+
+#include <seqan/seq_io.h>
+#include "../ig_tools/utils/string_tools.hpp"
+
+using seqan::Dna5String;
+using seqan::DnaQString;
+using seqan::SeqFileIn;
+using seqan::SeqFileOut;
+using seqan::CharString;
+
+void create_console_logger() {
+    using namespace logging;
+    logger *lg = create_logger("");
+    lg->add_writer(std::make_shared<console_writer>());
+    attach_logger(lg);
+}
 
 int main(int argc, char * argv[]){
+    create_console_logger();
     if(argc != 5 || strncmp(argv[1], "-i", 3) != 0 || strncmp(argv[3], "-o", 3) != 0) {
-        std::cout << "Extracts UMIs from fastq file into a separate one." << std::endl;
-        std::cout << "Usage: -i <input file> -o <output file>";
+        INFO("Extracts UMIs from fastq file into a separate one.");
+        INFO("Usage: -i <input file> -o <output file>");
         return 1;
     }
-    std::cout << "Extracting barcodes." << std::endl;
-    char *src_name = argv[2];
-    std::ifstream src(src_name);
-    assert(!src.fail());
-    char *dst_name = argv[4];
-    std::ofstream dst(dst_name);
-    std::size_t num_reads = 0;
-    while(!src.eof()){
-        std::string s;
-        std::getline(src, s);
-        if(s == "")
-            break;
+
+    INFO("Reading fastq");
+    SeqFileIn infile(argv[2]);
+    std::vector<CharString> input_ids;
+    {
+        std::vector<Dna5String> input_reads;
+        std::vector<DnaQString> input_qual;
+        readRecords(input_ids, input_reads, input_qual, infile);
+    }
+
+    INFO("Extracting barcodes.");
+    SeqFileOut outfile(argv[4]);
+    for (auto& id : input_ids) {
+        std::string s = seqan_string_to_string(id);
         std::size_t space = s.find(' ');
         assert(space != std::string::npos);
         space = s.find(' ', space + 1);
         assert(space != std::string::npos);
-        std::string id = s.substr(0, space);
+        std::string meta = s.substr(0, space);
         std::string umi_info = s.substr(space + 1);
         assert(!umi_info.empty());
-        dst << id << std::endl;
         std::size_t colon = umi_info.find(':');
         assert(colon != std::string::npos);
         umi_info = umi_info.substr(colon + 1);
         colon = umi_info.find(':');
         assert(colon != std::string::npos);
         assert(colon * 2 + 1 == umi_info.length());
-        dst << umi_info.substr(0, colon) << std::endl;
-        dst << "+" << std::endl;
-        dst << umi_info.substr(colon + 1) << std::endl;
-        num_reads++;
-
-        std::getline(src, s);
-        std::getline(src, s);
-        std::getline(src, s);
+        auto umi = umi_info.substr(0, colon);
+        auto qual = umi_info.substr(colon + 1);
+        writeRecord(outfile, meta, umi, qual);
     }
-    src.close();
-    dst.close();
-    std::cout << num_reads << " barcodes were extracted from " << src_name << " to " << dst_name << std::endl;
+
+    INFO(input_ids.size() << " barcodes were extracted from " << argv[2] << " to " << argv[4]);
 }
