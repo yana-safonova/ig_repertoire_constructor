c7c014bd3ea14b12daeacf36aff7dc788f13a6ec
Yana Safonova <safonova.yana@gmail.com>
2016-02-14 01:48:14 +0300

    accurate processing of supernodes during construction of the preliminary decomposition

--- src/dense_sgraph_finder/graph_decomposer/simple_decomposition_constructor.cpp
+++ src/dense_sgraph_finder/graph_decomposer/simple_decomposition_constructor.cpp
@@ -2,9 +2,14 @@
 
 using namespace dense_subgraph_finder;
 
+bool SimpleDecompositionConstructor::VertexIsSupernode(size_t vertex) {
+    return graph_ptr_->WeightOfVertex(vertex) >= min_supernode_size_;
+}
+
 void SimpleDecompositionConstructor::CreateFirstSet() {
     size_t first_index = permutation_prt_->Reverse()[0];
     decomposition_ptr_->SetClass(first_index, 0);
+    current_set_has_snode_ = VertexIsSupernode(first_index);
 }
 
 // vertex should be consistent with ids in hamming_graph_ptr
@@ -24,8 +29,8 @@ double SimpleDecompositionConstructor::ComputeEdgePercToPreviousSet(size_t verte
 }
 
 bool SimpleDecompositionConstructor::GlueVertexWithPreviousSet(size_t vertex) {
-    // if abundance of vertex is large, do not glue it
-    if(graph_ptr_->WeightOfVertex(vertex) >= min_supernode_size_)
+    // if vertex is supernode and class already contains supernode, do not glue it
+    if(graph_ptr_->WeightOfVertex(vertex) >= min_supernode_size_ and current_set_has_snode_)
         return false;
     double edge_perc = ComputeEdgePercToPreviousSet(vertex);
     TRACE("Edge %: " << edge_perc);
@@ -42,18 +47,21 @@ DecompositionPtr SimpleDecompositionConstructor::CreateDecomposition() {
         TRACE("Index: " << i);
         size_t old_perm_index = permutation_prt_->Reverse()[i];
         TRACE("Old perm index: " << old_perm_index);
-//        double edge_perc = ComputeEdgePercToPreviousSet(old_perm_index);
-//        TRACE("Edge %: " << edge_perc);
-//        assert(edge_perc <= 1.0);
-//        if(edge_perc >= edge_perc_threshold_) {
         if(GlueVertexWithPreviousSet(old_perm_index)) {
             decomposition_ptr_->SetClass(old_perm_index, cur_set);
             TRACE("Set " << cur_set << " was updated");
+            // if newly appended vertex is a supernode, set flag in true
+            if(VertexIsSupernode(old_perm_index))
+                current_set_has_snode_ = true;
         }
         else {
             cur_set++;
             decomposition_ptr_->SetClass(old_perm_index, cur_set);
             TRACE("New set was created");
+            // current class contains the only node: old_perm_index
+            // if it is a supernode, set flag in true
+            // otherwise in false
+            current_set_has_snode_ = VertexIsSupernode(old_perm_index);
         }
     }
     DEBUG(decomposition_ptr_->Size() << " classes were constructed");
