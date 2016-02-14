93788fb00230870a0a7c5903fc854349814baae9
Alexander Shlemov <shlemovalex@gmail.com>
2016-02-14 16:10:48 +0300

    Fix RCM in final_compressor

--- src/ig_quast_tool/ig_compress_equal_clusters.py
+++ src/ig_quast_tool/ig_compress_equal_clusters.py
@@ -23,11 +23,15 @@ if __name__ == "__main__":
     parser.add_argument("--tmp-file", "-T",
                         type=str,
                         default="",
-                        help="temporary file for ig_trie_compressor output (default: EMPTY)")
+                        help="temporary file for ig_trie_compressor output (default: <empty>)")
     parser.add_argument("--map-file", "-m",
                         type=str,
                         default="",
-                        help="map file (default: EMPTY)")
+                        help="map file (default: <empty>)")
+    parser.add_argument("--rcm", "-r",
+                        type=str,
+                        default="",
+                        help="rcm file for fixup, empty for skip this stage (default: <empty>)")
 
     args = parser.parse_args()
 
@@ -55,8 +59,22 @@ if __name__ == "__main__":
 
     with smart_open(args.tmp_file, "r") as fin, smart_open(args.output, "w") as fout:
         for i, record in enumerate(SeqIO.parse(fin, "fasta")):
-            record.id = record.description = "cluster___%d___size___%d" % (i + 1, final_mults[i])
+            record.id = record.description = "cluster___%d___size___%d" % (i, final_mults[i])
             SeqIO.write(record, fout, "fasta")
+
+
+    if args.rcm:
+        print "Fixing RCM file..."
+
+        with open(args.rcm, "r") as fin:
+            rcm = [line.strip().split() for line in fin]
+            rcm = [(id, int(cluster)) for id, cluster in rcm]
+
+        with open(args.rcm, "w") as fout:
+            for id, cluster in rcm:
+                target_cluster = targets[cluster]
+                fout.writelines("%s %s\n" % (id, target_cluster))
+
 
     print "Remove temporary files: %s %s" % (args.tmp_file, args.map_file)
     os.remove(args.tmp_file)
