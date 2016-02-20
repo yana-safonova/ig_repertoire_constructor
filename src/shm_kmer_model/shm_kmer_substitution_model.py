7cf4a7224b7140f4051b2203c6c5e8367f3641cd
seryrzu <seryrzu@gmail.com>
2016-02-20 15:20:53 +0300

    init class created

---
+++ src/shm_kmer_model/shm_kmer_substitution_model.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python2
+
+from __future__ import print_function
+from shm_kmer_substitution_model_estimator import SHM_KmerSubstitutionModelEstimator
+
+def main():
+    shm_kmer_substitution_model_estimator = SHM_KmerSubstitutionModelEstimator()
+
+
+if __name__ == '__main__':
+    main()
