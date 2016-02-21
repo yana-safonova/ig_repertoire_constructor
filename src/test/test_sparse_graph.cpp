4ba5a33532b00d4308465534604470997aa3f453
Alexander Shlemov <shlemovalex@gmail.com>
2016-02-21 21:14:17 +0300

    Add autotesting

--- src/test/test_sparse_graph.cpp
+++ src/test/test_sparse_graph.cpp
@@ -115,8 +115,8 @@ void SparseGraphTestFixture::SetUp() {
         }
     }
 }
-
-int main(int argc, char **argv) {
-    ::testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
-}
+//
+// int main(int argc, char **argv) {
+//     ::testing::InitGoogleTest(&argc, argv);
+//     return RUN_ALL_TESTS();
+// }
