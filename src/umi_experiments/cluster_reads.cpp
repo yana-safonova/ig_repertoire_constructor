2adb88786131bb3bd26f364e8c2773f8dd491d9d
Sergey Bankevich <Sergey.Bankevich@gmail.com>
2016-02-23 13:04:09 +0300

    make parallel + report intermediate stats

--- src/umi_experiments/cluster_reads.cpp
+++ src/umi_experiments/cluster_reads.cpp
@@ -1,18 +1,21 @@
+#include <atomic>
 #include <boost/program_options/options_description.hpp>
 #include <boost/program_options/parsers.hpp>
 #include <boost/program_options/variables_map.hpp>
+#include <openmp_wrapper.h>
 #include <segfault_handler.hpp>
 #include "utils.hpp"
 #include "umi_utils.hpp"
 #include "../fast_ig_tools/banded_half_smith_waterman.hpp"
 
-bool readArgs(int argc, char **argv, std::string& reads_file, std::string& output_file) {
+bool readArgs(int argc, char **argv, std::string& reads_file, std::string& output_file, unsigned& thread_count) {
     namespace po = boost::program_options;
     po::options_description cmdl_options("Is this needed?");
     cmdl_options.add_options()
             ("help,h", "print help message")
             ("reads,r", po::value<std::string>(&reads_file)->required(), "input file with reads")
             ("output,o", po::value<std::string>(&output_file)->default_value(""), "output file with stats")
+            ("threads,t", po::value<unsigned>(&thread_count)->default_value(1), "number of running threads")
             ;
     po::variables_map vm;
     po::store(po::command_line_parser(argc, argv).options(cmdl_options).run(), vm);
@@ -59,14 +62,14 @@ public:
 
     std::string ToString();
 
-    static Stats GetStats(const std::vector<seqan::Dna5String>& input_reads, const std::unordered_map<Umi, std::vector<size_t> >& umi_to_reads);
+    static Stats GetStats(const std::vector<seqan::Dna5String>& input_reads, const std::unordered_map<Umi, std::vector<size_t> >& umi_to_reads, unsigned thread_count);
 
 private:
     std::vector<size_t> hamming_dist_distribution_;
     std::vector<size_t> sw_dist_distribution_;
 };
 
-Stats Stats::GetStats(const std::vector<seqan::Dna5String>& input_reads, const std::unordered_map<Umi, std::vector<size_t> >& umi_to_reads) {
+Stats Stats::GetStats(const std::vector<seqan::Dna5String>& input_reads, const std::unordered_map<Umi, std::vector<size_t> >& umi_to_reads, unsigned thread_count) {
     size_t max_read_length = 0;
     for (auto& read : input_reads) {
         max_read_length = std::max(max_read_length, length(read));
@@ -76,26 +79,40 @@ Stats Stats::GetStats(const std::vector<seqan::Dna5String>& input_reads, const s
         return static_cast<size_t>(-half_sw_banded(s1, s2, 0, -1, -1, [](int) -> int { return 0; }, 0));
     };
 
+    std::vector<std::vector<size_t>> read_groups;
+    for (auto& entry : umi_to_reads) {
+        read_groups.push_back(entry.second);
+    }
     std::vector<size_t> hamming_dist_distribution(max_read_length + 1);
     std::vector<size_t> sw_dist_distribution(max_read_length + 1);
-    size_t processed = 0;
-    size_t next_ten = 1;
-    for (auto& entry : umi_to_reads) {
-        auto& reads = entry.second;
+    std::atomic<size_t> processed;
+    processed = 0;
+    size_t next_percent = 1;
+    omp_set_num_threads(thread_count);
+#pragma omp parallel for schedule(dynamic)
+    for (size_t group = 0; group < read_groups.size(); group ++) {
+        auto& reads = read_groups[group];
         for (size_t i = 0; i < reads.size(); i++) {
             for (size_t j = 0; j < i; j++) {
                 auto& first = input_reads[reads[i]];
                 auto& second = input_reads[reads[j]];
                 size_t hamming_dist = get_hamming_dist(first, second);
                 size_t sw_dist = get_sw_dist(first, second);
-                hamming_dist_distribution[hamming_dist]++;
-                sw_dist_distribution[sw_dist]++;
+#pragma omp critical
+                {
+                    hamming_dist_distribution[hamming_dist] ++;
+                    sw_dist_distribution[sw_dist] ++;
+                }
             }
         }
         processed += reads.size();
-        while (processed * 100 >= input_reads.size() * next_ten * 10) {
-            INFO(next_ten * 10 << "% of " << input_reads.size() << " reads processed");
-            next_ten ++;
+        while (processed * 100 >= input_reads.size() * next_percent) {
+#pragma omp critical
+            if (processed * 100 >= input_reads.size() * next_percent) {
+                INFO(next_percent << "% of " << input_reads.size() << " reads processed");
+                next_percent++;
+                std::cout << Stats(hamming_dist_distribution, sw_dist_distribution).ToString();
+            }
         }
     }
     return Stats(hamming_dist_distribution, sw_dist_distribution);
@@ -120,12 +137,23 @@ std::string Stats::ToString() {
     return ss.str();
 }
 
+void print_umi_reads_distribution_by_size(unordered_map<Umi, vector<size_t>>& umi_to_reads) {
+    std::map<size_t, size_t> size_to_count;
+    for (auto& entry : umi_to_reads) {
+        size_to_count[entry.second.size()] ++;
+    }
+    for (auto& entry : size_to_count) {
+        std::cout << "size " << entry.first << ", count " << entry.second << std::endl;
+    }
+}
+
 int main(int argc, char **argv) {
     segfault_handler sh;
     create_console_logger();
     std::string reads_file;
     std::string output_file;
-    if (!readArgs(argc, argv, reads_file, output_file)) {
+    unsigned thread_count;
+    if (!readArgs(argc, argv, reads_file, output_file, thread_count)) {
         return 0;
     }
 
@@ -146,10 +174,11 @@ int main(int argc, char **argv) {
     INFO("Grouping reads by barcodes");
     std::unordered_map<Umi, std::vector<size_t> > umi_to_reads;
     group_reads_by_umi(umis, umi_to_reads);
-    INFO(umi_to_reads.size() << " unique barcodes found");
+    INFO(umi_to_reads.size() << " unique barcodes found, stats by sizes:");
+    print_umi_reads_distribution_by_size(umi_to_reads);
 
     INFO("Calculating stats");
-    auto stats = Stats::GetStats(input_reads, umi_to_reads);
+    auto stats = Stats::GetStats(input_reads, umi_to_reads, thread_count);
 
     auto string_stats = stats.ToString();
     if (output_file != "") {
