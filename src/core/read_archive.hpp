9aebf01881f777bf32023a913be8e7c93bbd11e4
Yana Safonova <safonova.yana@gmail.com>
2016-03-29 00:39:12 +0300

    draft of core module

---
+++ src/core/read_archive.hpp
@@ -0,0 +1,52 @@
+#pragma once
+
+#include <unordered_map>
+#include <seqan/sequence.h>
+
+namespace core {
+
+    struct Read {
+        std::string name;
+        seqan::Dna5String seq;
+        size_t id;
+
+        Read(std::string new_name,
+             seqan::Dna5String new_seq,
+             size_t new_id) :
+                name(new_name),
+                seq(new_seq),
+                id(new_id) { }
+
+        Read() : name(), seq() { }
+    };
+
+    std::ostream &operator<<(std::ostream &out, const Read &read);
+
+    typedef std::shared_ptr <Read> ReadPtr;
+
+//-----------------------------------------------------------
+
+    class ReadArchive {
+        std::vector <Read> reads_;
+        std::unordered_map <std::string, size_t> name_index_map_;
+
+    public:
+        ReadArchive(std::string fastq_file_fname);
+
+        size_t size() const;
+
+        typedef std::vector<Read>::const_iterator read_iterator;
+
+        read_iterator cbegin() const { return reads_.cbegin(); }
+
+        read_iterator cend() const { return reads_.cend(); }
+
+        const Read& operator[](size_t index) const;
+
+        const Read& GetReadByName(std::string read_name) const;
+
+        size_t GetIndexByReadName(std::string read_name) const;
+
+        void FixSpacesInHeaders();
+    };
+}
