5b54ccad952383d10ec90bdbffde3ef4c819dce0
Yana Safonova <safonova.yana@gmail.com>
2016-05-18 04:00:01 +0300

    output was added, odd config fields were renamed

--- src/vj_finder/vj_finder_config.cpp
+++ src/vj_finder/vj_finder_config.cpp
@@ -42,10 +42,10 @@ namespace vj_finder {
 
     void update_output_files_config(VJFinderConfig::IOParams::OutputParams::OutputFiles & of) {
         of.log_filename = path::append_path(of.output_dir, of.log_filename);
-        of.add_info_filename = path::append_path(of.output_dir, of.add_info_filename);
-        of.bad_output_filename = path::append_path(of.output_dir, of.bad_output_filename);
+        of.alignment_info_fname = path::append_path(of.output_dir, of.alignment_info_fname);
+        of.filtered_reads_fname = path::append_path(of.output_dir, of.filtered_reads_fname);
         of.discard_info_filename = path::append_path(of.output_dir, of.discard_info_filename);
-        of.output_filename = path::append_path(of.output_dir, of.output_filename);
+        of.cleaned_reads_fname = path::append_path(of.output_dir, of.cleaned_reads_fname);
         of.valignments_filename = path::append_path(of.output_dir, of.valignments_filename);
     }
 
@@ -53,11 +53,11 @@ namespace vj_finder {
               boost::property_tree::ptree const &pt, bool) {
         using config_common::load;
         load(of.log_filename, pt, "log_filename");
-        load(of.add_info_filename, pt, "add_info_filename");
-        load(of.bad_output_filename, pt, "bad_output_filename");
+        load(of.alignment_info_fname, pt, "alignment_info_fname");
+        load(of.filtered_reads_fname, pt, "filtered_reads_fname");
         load(of.discard_info_filename, pt, "discard_info_filename");
         load(of.output_dir, pt, "output_dir");
-        load(of.output_filename, pt, "output_filename");
+        load(of.cleaned_reads_fname, pt, "cleaned_reads_fname");
         load(of.valignments_filename, pt, "valignments_filename");
         update_output_files_config(of);
     }
