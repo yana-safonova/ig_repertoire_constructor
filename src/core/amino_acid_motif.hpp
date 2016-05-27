6acca2c8c2530efd0c115afc6b477c2341cc2f55
Yana Safonova <safonova.yana@gmail.com>
2016-05-27 21:24:58 +0300

    Some AA utils

---
+++ src/core/amino_acid_motif.hpp
@@ -0,0 +1,33 @@
+#pragma once
+
+#include <vector>
+#include <string>
+
+namespace core {
+    // X means unknown amino acid
+    std::string AminoAcidExtendedAlphabet = "ACDEFGHIKLMNPQRSTVWYX";
+
+    bool CharIsExtendedAminoAcid(char sym);
+
+    bool StringIsExtendedAminoAcid(std::string aa_seq);
+
+    class AminoAcidMotifs {
+        std::string aa_motif_;
+        std::vector<std::string> aa_variants_;
+
+        void Initialize();
+
+    public:
+        AminoAcidMotifs(std::string aa_motif) : aa_motif_(aa_motif) {
+            Initialize();
+        }
+
+        size_t size() const { return aa_variants_.size(); }
+
+        typedef std::vector<std::string>::const_iterator AaVariantsConstIterator;
+
+        AaVariantsConstIterator cbegin() const { return aa_variants_.begin(); }
+
+        AaVariantsConstIterator cend() const { return aa_variants_.end(); }
+    };
+}
