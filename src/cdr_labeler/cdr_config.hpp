661f7d893e5265597360fc63d1dd2323985d1144
Yana Safonova <safonova.yana@gmail.com>
2016-05-29 23:53:16 +0300

    CDR labeler: WIP

--- src/cdr_labeler/cdr_config.hpp
+++ src/cdr_labeler/cdr_config.hpp
@@ -18,6 +18,22 @@ namespace cdr_labeler {
         };
 
         struct CDRsParams {
+            struct AnnotatedSearchParams {
+                enum DomainSystem { UnknownDomain, IMGT, Kabat };
+                DomainSystem domain_system;
+                std::string imgt_v_annotation;
+                std::string kabat_v_annotation;
+                std::string imgt_j_annotation;
+                std::string kabat_j_annotation;
+                size_t v_gene_line_index;
+                size_t cdr1_start_line_index;
+                size_t cdr1_end_line_index;
+                size_t cdr2_start_line_index;
+                size_t cdr2_end_line_index;
+                size_t fr3_end_index;
+
+            };
+
             struct HCDR1Params {
                 size_t start_pos;
                 size_t start_shift;
@@ -45,9 +61,13 @@ namespace cdr_labeler {
                 std::string residues_after;
             };
 
+            AnnotatedSearchParams annotated_search_params;
             HCDR1Params hcdr1_params;
             HCDR2Params hcdr2_params;
             HCDR3Params hcdr3_params;
+
+            enum CDRSearchAlgorithm { UnknownSearchAlgorithm, AnnotatedSearch, DeNovoSearch };
+            CDRSearchAlgorithm cdr_search_algorithm;
         };
 
         InputParams input_params;
