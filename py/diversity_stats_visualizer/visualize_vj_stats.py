f19920352a46949baa18e4d6b0a0efba70bb602a
Yana Safonova <safonova.yana@gmail.com>
2016-06-15 01:58:33 +0300

    refactoring visualizing tools

--- py/diversity_stats_visualizer/visualize_vj_stats.py
+++ py/diversity_stats_visualizer/visualize_vj_stats.py
@@ -16,6 +16,8 @@ from Bio import SeqIO
 from Bio.Seq import Seq
 from Bio.SeqRecord import SeqRecord
 
+import visualize_cdr_stats
+
 class VJMatrix:
     def __init__(self, v_hits, j_hits):
         self.num_records = len(v_hits)
@@ -95,145 +97,7 @@ def visualize_vj_heatmap(labeling_df, output_pdf):
 
 ############################################################################
 
-def visualize_region_lengths(labeling_df, region, region_name, output_fname):
-    region_seq = list(labeling_df[region])
-    region_len = [len(s) for s in region_seq if len(s) > 1]
-    sns.distplot(region_len, kde = False, rug=False)
-    plt.xlabel(region_name + ' length', fontsize = 16)
-    plt.ylabel('# ' + region_name + 's', fontsize = 16)
-    plt.xticks(fontsize = 14)
-    plt.yticks(fontsize = 14)
-    pp = PdfPages(output_fname)
-    pp.savefig()
-    pp.close()
-    plt.clf()
-    print region_name + " length distribution was written to " + output_fname
 
-############################################################################
-
-def get_region_largest_group(region_seq):
-    len_dict = dict()
-    for s in region_seq:
-        if len(s) not in len_dict:
-            len_dict[len(s)] = list()
-        len_dict[len(s)].append(s)
-    max_len = 0
-    max_group = 0
-    for l in len_dict:
-        if len(len_dict[l]) > max_group:
-            max_group = len(len_dict[l])
-            max_len = l
-    return len_dict[max_len]
-
-def get_nucls_lists(max_group):
-    region_len = len(max_group[0])
-    nucl_dict = dict()
-    nucl_dict['A'] = [0] * region_len
-    nucl_dict['C'] = [0] * region_len
-    nucl_dict['G'] = [0] * region_len
-    nucl_dict['T'] = [0] * region_len
-    for s in max_group:
-        for i in range(0, len(s)):
-            if s[i] in nucl_dict:
-                nucl_dict[s[i]][i] += 1
-    for i in range(0, region_len):
-        sum = nucl_dict['A'][i] + nucl_dict['C'][i] + nucl_dict['G'][i] + nucl_dict['T'][i]
-        nucl_dict['A'][i] = float(nucl_dict['A'][i]) / float(sum) * 100
-        nucl_dict['C'][i] = float(nucl_dict['C'][i]) / float(sum) * 100
-        nucl_dict['G'][i] = float(nucl_dict['G'][i]) / float(sum) * 100
-        nucl_dict['T'][i] = float(nucl_dict['T'][i]) / float(sum) * 100
-    nucl_dict['A'] = np.array(nucl_dict['A'])
-    nucl_dict['C'] = np.array(nucl_dict['C'])
-    nucl_dict['G'] = np.array(nucl_dict['G'])
-    nucl_dict['T'] = np.array(nucl_dict['T'])
-    return nucl_dict
-
-def visualize_largest_region_nucls(labeling_df, region, region_name, output_fname):
-    region_seq = list(labeling_df[region])
-    max_group = get_region_largest_group(region_seq)
-    if len(max_group) == 0:
-        return
-    nucl_dict = get_nucls_lists(max_group)
-    x = range(0, len(max_group[0]))
-    x_l = [str(i) for i in range(1, len(max_group[0]) + 1)]
-    acgt = nucl_dict['A'] + nucl_dict['C'] + nucl_dict['G'] + nucl_dict['T']
-    cgt = nucl_dict['C'] + nucl_dict['G'] + nucl_dict['T']
-    gt = nucl_dict['G'] + nucl_dict['T']
-    #sns.set_color_codes("pastel")
-    sns.set_color_codes("muted")
-    f, ax = plt.subplots(figsize=(15, 6))
-    sns.barplot(x=x, y=acgt, label="A", color = 'b')
-    sns.barplot(x=x, y=cgt, label="C", color = 'g')
-    sns.barplot(x=x, y=gt, label="G", color = 'r')
-    sns.barplot(x=x, y=nucl_dict['T'], label="T", color = 'orange')
-    ax.legend(ncol = 4, loc="upper center", frameon=True, fontsize = 16)
-    plt.xlabel(region_name + ' position', fontsize = 16)
-    plt.ylabel('Nucleotide %', fontsize = 16)
-    plt.xticks(x, x_l, fontsize = 14)
-    plt.yticks(fontsize = 14)
-    pp = PdfPages(output_fname)
-    pp.savefig()
-    pp.close()
-    plt.clf()
-    print region_name + " nucleotide distribution was written to " + output_fname
-
-############################################################################
-
-np.random.seed(1)
-cm = plt.get_cmap('gnuplot2')
-aa_colors = []
-for i in range(21):
-    aa_colors.append(cm(float(i + 1) / 21)) #(np.random.random_sample()))
-amino_acids = ['A', 'G', 'L', 'R', 'W', 'N', 'V', 'I', 'P', 'F', 'Y', 'C', 'T', 'S', 'M', 'Q', 'K', 'H', 'D', 'E', '*']
-
-def visualize_largest_group_aa_variability(labeling_df, region, region_name, output_fname):
-    region_seq = list(labeling_df[region])
-    max_group = get_region_largest_group(region_seq)
-    if len(max_group) == 0:
-        return
-    group_len = len(max_group[0])
-    if group_len % 3 != 0:
-        print "Largest " + region_name + " is not out-of-frame"
-        return
-    aa_seqs = [Seq(cdr).translate(to_stop=True) for cdr in max_group]
-    aa_list = [dict() for i in range(0, group_len / 3)]
-    for aa_seq in aa_seqs:
-        for i in range(0, len(aa_seq)):
-            if aa_seq[i] not in aa_list[i]:
-                aa_list[i][aa_seq[i]] = 0
-            aa_list[i][aa_seq[i]] += 1
-    aa_num = [len(aa) for aa in aa_list]
-    aa_large_abun = []
-    aa_large_acid = []
-    for aa in aa_list:
-        aa = sorted(aa.items(), key=operator.itemgetter(1), reverse = True)
-        sum = 0
-        for i in aa:
-            sum += i[1]
-        aa_large_abun.append(float(aa[0][1]) / float(sum) * 100)
-        aa_large_acid.append(aa[0][0])
-    aa_set = set()
-    for aa in aa_large_acid:
-        aa_set.add(aa)
-    for aa in aa_set:
-        x_ = []
-        abun_ = []
-        for i in range(0, len(aa_large_abun)):
-            x_.append(i)
-            if aa_large_acid[i] == aa:
-                abun_.append(aa_large_abun[i])
-            else:
-                abun_.append(0)
-        sns.barplot(x_, abun_, color = aa_colors[amino_acids.index(aa)])
-    plt.xticks(range(0, len(aa_large_abun)), aa_large_acid, fontsize = 14)
-    plt.yticks(fontsize = 14)
-    plt.xlabel('The most abundant amino acid', fontsize = 16)
-    plt.ylabel('% ' + region_name + 's', fontsize = 16)
-    pp = PdfPages(output_fname)
-    pp.savefig()
-    pp.close()
-    plt.clf()
-    print region_name + " aa variability was written to " + output_fname
 
 ############################################################################
 
@@ -393,22 +257,15 @@ def main(argv):
         return
     vj_df = pd.read_table(argv[1], delim_whitespace = True)
     v_alignment_fa = argv[2]
-    shms_df = pd.read_table(argv[3], delim_whitespace = True)
     output_dir = argv[4]
     checkout_output_dir(output_dir)
     visualize_vj_heatmap(vj_df, os.path.join(output_dir, "vj_heatmap.pdf"))
-    visualize_region_lengths(vj_df, "CDR1_nucls", "CDR1", os.path.join(output_dir, "cdr1_length.pdf"))
-    visualize_region_lengths(vj_df, "CDR2_nucls", "CDR2", os.path.join(output_dir, "cdr2_length.pdf"))
-    visualize_region_lengths(vj_df, "CDR3_nucls", "CDR3", os.path.join(output_dir, "cdr3_length.pdf"))
-    visualize_largest_region_nucls(vj_df, "CDR1_nucls", "CDR1", os.path.join(output_dir, "cdr1_nucls.pdf"))
-    visualize_largest_region_nucls(vj_df, "CDR2_nucls", "CDR2", os.path.join(output_dir, "cdr2_nucls.pdf"))
-    visualize_largest_region_nucls(vj_df, "CDR3_nucls", "CDR3", os.path.join(output_dir, "cdr3_nucls.pdf"))
-    visualize_largest_group_aa_variability(vj_df, "CDR1_nucls", "CDR1", os.path.join(output_dir, "cdr1_aa.pdf"))
-    visualize_largest_group_aa_variability(vj_df, "CDR2_nucls", "CDR2", os.path.join(output_dir, "cdr2_aa.pdf"))
-    visualize_largest_group_aa_variability(vj_df, "CDR3_nucls", "CDR3", os.path.join(output_dir, "cdr3_aa.pdf"))
+    visualize_cdr_stats.main(argv[1], output_dir)
     visualize_v_mutations_stats(v_alignment_fa, os.path.join(output_dir, "v_mutations_distribution.pdf"))
-    visualize_aa_substitutions(shms_df, os.path.join(output_dir, "aa_substitution_heatmap.pdf"))
-    visualize_special_shm_positions(shms_df, os.path.join(output_dir, "synonymous_stop_codon_shms.pdf"))
+
+    #shms_df = pd.read_table(argv[3], delim_whitespace = True)
+    #visualize_aa_substitutions(shms_df, os.path.join(output_dir, "aa_substitution_heatmap.pdf"))
+    #visualize_special_shm_positions(shms_df, os.path.join(output_dir, "synonymous_stop_codon_shms.pdf"))
 
 if __name__ == "__main__":
     main(sys.argv)
