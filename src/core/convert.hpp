5d8c2ef202727148562511e58a8eda4241980781
Yana Safonova <safonova.yana@gmail.com>
2016-07-21 19:53:32 +0300

    versatile to string converter

--- src/core/convert.hpp
+++ src/core/convert.hpp
@@ -15,4 +15,11 @@ namespace core {
     };
 
     std::string dna5String_to_string(seqan::Dna5String);
+
+    template<typename SeqanStringType>
+    std::string seqan_string_to_string(SeqanStringType seqan_str) {
+        std::stringstream ss;
+        ss << seqan_str;
+        return ss.str();
+    }
 }
