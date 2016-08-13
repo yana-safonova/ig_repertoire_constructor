7bc791b2684595bc9f9acfd450d9eec266f0281b
Alexander Shlemov <shlemovalex@gmail.com>
2016-08-13 23:22:49 +0300

    Rewrite benchmark script

---
+++ py/test_on_barcoded_datasets.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python2
+
+from test_on_naive_simulation import *
+
+
+def run_and_quast_all_three(input_dir, threads=16):
+    for i in [3]:
+        run_and_quast_all(input_dir + "/input%d.fa.gz" % i,
+                          input_dir + "/repertoire%d.fa.gz" % i,
+                          input_dir + "/repertoire%d.rcm" % i,
+                          out_dir=input_dir + "/filtering%d" % i,
+                          threads=threads)
+
+
+if __name__ == "__main__":
+    import os.path
+
+    datasets = ["AGE1", "AGE2", "AGE3", "AGE4", "AGE5", "AGE6", "AGE7", "AGE8", "AGE9", "MG91M", "HD09M"]
+    datasets = ["AGE3", "MG91M", "HD09M", "AGE7"]
+    # datasets = ["AGE3", "MG91M", "HD09M"]
+    datasets = ["AGE3"]
+    datasets = []
+    datasets = ["FLU_FV_27"]
+    datasets = ["FLU_FV_21", "FLU_FV_22", "FLU_FV_23", "FLU_FV_27"]
+
+    for dataset in datasets:
+        ddir = igrec_dir + "/src/extra/ig_quast_tool/" + dataset
+        if os.path.isfile(ddir + "/input1.fa.gz"):
+            run_and_quast_all_three(ddir)
--- py/test_on_naive_simulation.py
+++ py/test_on_naive_simulation.py
@@ -2,8 +2,6 @@
 
 from simulate import *
 from aimquast_impl import get_clusters_sizes
-import os
-
 
 path_to_aimquast = igrec_dir + "/aimquast.py"
 
@@ -13,117 +11,76 @@ def run_and_quast_all(input_reads,
                       ideal_repertoire_rcm,
                       out_dir,
                       threads=4,
-                      rerun_mixcr=False,
+                      rerun_mixcr=True,
                       do_not_run=False):
     import os.path
+    import shutil
 
     mkdir_p(out_dir)
 
+    class IgReCRun:
+
+        def __init__(self,
+                     name,
+                     tau=3,
+                     fillin=0.6,
+                     trivial=False,
+                     loci="all"):
+            self.name = name
+            self.tau = tau
+            self.fillin = fillin
+            self.trivial = trivial
+            self.loci = loci
+
+        def run(self):
+            run_igrec(input_reads,
+                      tau=self.tau,
+                      additional_args=" --create-triv-dec --no-alignment" if self.trivial else " --no-alignment",
+                      min_fillin=self.fillin,
+                      threads=threads,
+                      loci=self.loci,
+                      output_dir=out_dir + "/" + self.name + "/")
+
+    igrec_runs = []
+    igrec_runs.append(IgReCRun("igrec_trivial", trivial=True))
+
+    igrec_runs.append(IgReCRun("igrec"))
+    igrec_runs.append(IgReCRun("igrec_f03", fillin=0.3))
+    # igrec_runs.append(IgReCRun("igrec_f075", fillin=0.75))
+    igrec_runs.append(IgReCRun("igrec_f09", fillin=0.9))
+    # igrec_runs.append(IgReCRun("igrec_f095", fillin=0.95))
+    igrec_runs.append(IgReCRun("igrec_tau3", tau=3))
+    igrec_runs.append(IgReCRun("igrec_tau3_f03", tau=3, fillin=0.3))
+    # igrec_runs.append(IgReCRun("igrec_tau3_f075", tau=3, fillin=0.75))
+    igrec_runs.append(IgReCRun("igrec_tau3_f09", tau=3, fillin=0.9))
+    # igrec_runs.append(IgReCRun("igrec_tau3_f095", tau=3, fillin=0.95))
+    igrec_runs.append(IgReCRun("igrec_tau2", tau=2))
+    igrec_runs.append(IgReCRun("igrec_tau2_f03", tau=2, fillin=0.3))
+    # igrec_runs.append(IgReCRun("igrec_tau2_f075", tau=2, fillin=0.75))
+    igrec_runs.append(IgReCRun("igrec_tau2_f09", tau=2, fillin=0.9))
+    # igrec_runs.append(IgReCRun("igrec_tau2_f095", tau=2, fillin=0.95))
+    igrec_runs.append(IgReCRun("igrec_tau1", tau=1))
+    igrec_runs.append(IgReCRun("igrec_tau1_f03", tau=1, fillin=0.3))
+    # igrec_runs.append(IgReCRun("igrec_tau1_f075", tau=1, fillin=0.75))
+    igrec_runs.append(IgReCRun("igrec_tau1_f09", tau=1, fillin=0.9))
+    # igrec_runs.append(IgReCRun("igrec_tau1_f095", tau=1, fillin=0.95))
+
     if not do_not_run:
-        run_igrec(input_reads,
-                  threads=threads,
-                  output_dir=out_dir + "/igrec/")
-
-        run_igrec(input_reads,
-                  additional_args=" --create-triv-dec",
-                  threads=threads,
-                  output_dir=out_dir + "/igrec_trivial/")
-
-        run_igrec(input_reads,
-                  min_fillin=0.3,
-                  threads=threads,
-                  output_dir=out_dir + "/igrec_f03/")
-
-        run_igrec(input_reads,
-                  tau=3,
-                  threads=threads,
-                  output_dir=out_dir + "/igrec_tau3/")
-
-        run_igrec(input_reads,
-                  tau=3,
-                  min_fillin=0.3,
-                  threads=threads,
-                  output_dir=out_dir + "/igrec_tau3_f03/")
-
-        run_igrec(input_reads,
-                  tau=3,
-                  min_fillin=0.75,
-                  threads=threads,
-                  output_dir=out_dir + "/igrec_tau3_f075/")
+        for run in igrec_runs:
+            run.run()
 
         if rerun_mixcr or not os.path.isfile(out_dir + "/mixcr/final_repertoire.fa"):
-            run_mixcr(input_reads, threads=threads, output_dir=out_dir + "/mixcr/")
+            run_mixcr(input_reads, threads=threads, output_dir=out_dir + "/mixcr/", loci="all")
 
-        # run_igrec(input_reads,
-        #           additional_args=" --create-triv-dec",
-        #           threads=threads,
-        #           output_dir=out_dir + "/igrec_trivial/")
-        #
-        # run_igrec(input_reads,
-        #           min_fillin=0.3,
-        #           threads=threads,
-        #           output_dir=out_dir + "/igrec_f03/")
-        #
-        # run_igrec(input_reads,
-        #           tau=3,
-        #           threads=threads,
-        #           output_dir=out_dir + "/igrec_tau3/")
-        #
-        # run_igrec(input_reads,
-        #           tau=3,
-        #           min_fillin=0.3,
-        #           threads=threads,
-        #           output_dir=out_dir + "/igrec_tau3_f03/")
-        #
-        # run_igrec(input_reads,
-        #           tau=3,
-        #           min_fillin=0.75",
-        #           threads=threads,
-        #           output_dir=out_dir + "/igrec_tau3_f075/")
-        #
-        # if rerun_mixcr or not os.path.isfile(out_dir + "/mixcr/final_repertoire.fa"):
-        #     run_mixcr(input_reads, threads=threads, output_dir=out_dir + "/mixcr/")
+        mkdir_p(out_dir + "/supernode")
+        shutil.copy(out_dir + "/igrec/supernode_repertoire.fa",
+                    out_dir + "/supernode/final_repertoire.fa")
+        shutil.copy(out_dir + "/igrec/supernode_repertoire.rcm",
+                    out_dir + "/supernode/final_repertoire.rcm")
 
-        run_igrec(input_reads,
-                  tau=3,
-                  min_fillin=0.9,
-                  threads=threads,
-                  output_dir=out_dir + "/igrec_tau3_f09/")
+    kinds = [run.name for run in igrec_runs] + ["supernode", "mixcr"]
 
-        run_igrec(input_reads,
-                  tau=3,
-                  min_fillin=0.95,
-                  threads=threads,
-                  output_dir=out_dir + "/igrec_tau3_f095/")
-
-        run_igrec(input_reads,
-                  tau=2,
-                  min_fillin=0.6,
-                  threads=threads,
-                  output_dir=out_dir + "/igrec_tau2_f06/")
-
-        run_igrec(input_reads,
-                  tau=2,
-                  min_fillin=0.9,
-                  threads=threads,
-                  output_dir=out_dir + "/igrec_tau2_f09/")
-
-        run_igrec(input_reads,
-                  tau=2,
-                  min_fillin=0.3,
-                  threads=threads,
-                  output_dir=out_dir + "/igrec_tau2_f03/")
-
-        run_igrec(input_reads,
-                  tau=1,
-                  min_fillin=0.6,
-                  threads=threads,
-                  output_dir=out_dir + "/igrec_tau1_f06/")
-
-    kinds = ["igrec", "igrec_trivial", "igrec_f03", "igrec_tau3", "igrec_tau3_f03", "igrec_tau3_f075", "mixcr"]
-    kinds2 = ["igrec_tau3_f09", "igrec_tau3_f095", "igrec_tau2_f06", "igrec_tau2_f09", "igrec_tau2_f03", "igrec_tau1_f06"]
-
-    for kind in kinds + kinds2:
+    for kind in kinds:
         args = {"ideal_repertoire_fa": ideal_repertoire_fa,
                 "ideal_repertoire_rcm": ideal_repertoire_rcm,
                 "input_reads": input_reads,
@@ -138,41 +95,7 @@ def run_and_quast_all(input_reads,
         os.system(cmd)
 
 
-def run_and_quast_all_three(input_dir, threads=16):
-    # run_and_quast_all(input_dir + "/input1.fa.gz",
-    #                   input_dir + "/repertoire1.fa.gz",
-    #                   input_dir + "/repertoire1.rcm",
-    #                   out_dir=input_dir + "/filtering1",
-    #                   threads=threads)
-    #
-    # run_and_quast_all(input_dir + "/input2.fa.gz",
-    #                   input_dir + "/repertoire2.fa.gz",
-    #                   input_dir + "/repertoire2.rcm",
-    #                   out_dir=input_dir + "/filtering2",
-    #                   threads=threads)
-
-    run_and_quast_all(input_dir + "/input3.fa.gz",
-                      input_dir + "/repertoire3.fa.gz",
-                      input_dir + "/repertoire3.rcm",
-                      out_dir=input_dir + "/filtering3",
-                      threads=threads)
-
-
 if __name__ == "__main__":
-    import os.path
-
-    datasets = ["AGE1", "AGE2", "AGE3", "AGE4", "AGE5", "AGE6", "AGE7", "AGE8", "AGE9", "MG91M", "HD09M"]
-    datasets = ["AGE3", "MG91M", "HD09M", "AGE7"]
-    # datasets = ["AGE3", "MG91M", "HD09M"]
-    datasets = ["AGE3"]
-    datasets = []
-    datasets = ["FLU_FV_27"]
-    datasets = ["FLU_FV_21", "FLU_FV_22", "FLU_FV_23", "FLU_FV_27"]
-    for dataset in datasets:
-        ddir = igrec_dir + "/src/extra/ig_quast_tool/" + dataset
-        if os.path.isfile(ddir + "/input1.fa.gz"):
-            run_and_quast_all_three(ddir)
-
     ig_simulator_output_dir = "/tmp/ig_simulator"
     output_dir = igrec_dir + "/various_error_rate"
     run_ig_simulator(ig_simulator_output_dir,
@@ -193,7 +116,6 @@ if __name__ == "__main__":
             sizes = get_clusters_sizes(out_dir + "/ideal_final_repertoire.fa")
             print "Reference consists of %d clusters" % len(sizes)
             print "Reference consists of %d large (>=5) clusters" % len([size for size in sizes if size >= 5])
-            sys.exit()
 
             run_and_quast_all(out_dir + "/merged_reads.fa",
                               out_dir + "/ideal_final_repertoire.fa",
