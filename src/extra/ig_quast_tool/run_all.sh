7387f0a211c40676b1c433ff5b51e054b8998267
Alexander Shlemov <shlemovalex@gmail.com>
2016-08-15 01:36:44 +0300

    WIP on testing

--- src/extra/ig_quast_tool/run_all.sh
+++ src/extra/ig_quast_tool/run_all.sh
@@ -11,14 +11,8 @@ do
     wait
 done
 
-exit
-
 for i in 1 2 3 4 5 6 7 8 9
-# for i in 4
 do
     bash make_reference.sh /Nancy/data/input/ImmunoSeq/ibh_datasets/age_datasets/raw_reads/merged_reads/age_ig_s${i}_R12.fastq AGE${i} &
 done
 wait
-
-wait
-
