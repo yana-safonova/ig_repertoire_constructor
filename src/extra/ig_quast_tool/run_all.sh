2034872adbcfa10ebb04ad0f0c6a10dc3f0e7893
Alexander Shlemov <shlemovalex@gmail.com>
2016-08-14 04:44:40 +0300

    Make separate references for different loci

--- src/extra/ig_quast_tool/run_all.sh
+++ src/extra/ig_quast_tool/run_all.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
-bash make_reference.sh /Nancy/data/input/ImmunoSeq/AbVitro/flu_time_course/FV/single_reads_with_UMIs/FV_21.fastq  FLU_FV_21 &
-bash make_reference.sh /Nancy/data/input/ImmunoSeq/AbVitro/flu_time_course/FV/single_reads_with_UMIs/FV_22.fastq  FLU_FV_22 &
-bash make_reference.sh /Nancy/data/input/ImmunoSeq/AbVitro/flu_time_course/FV/single_reads_with_UMIs/FV_23.fastq  FLU_FV_23 &
-bash make_reference.sh /Nancy/data/input/ImmunoSeq/AbVitro/flu_time_course/FV/single_reads_with_UMIs/FV_27.fastq  FLU_FV_27 &
-wait
+for LOCI in IG IGH IGL IGK
+do
+    bash make_reference.sh /Nancy/data/input/ImmunoSeq/AbVitro/flu_time_course/FV/single_reads_with_UMIs/FV_21.fastq  FLU_FV_21_${LOCI} $LOCI &
+    bash make_reference.sh /Nancy/data/input/ImmunoSeq/AbVitro/flu_time_course/FV/single_reads_with_UMIs/FV_22.fastq  FLU_FV_22_${LOCI} $LOCI &
+    bash make_reference.sh /Nancy/data/input/ImmunoSeq/AbVitro/flu_time_course/FV/single_reads_with_UMIs/FV_23.fastq  FLU_FV_23_${LOCI} $LOCI &
+    bash make_reference.sh /Nancy/data/input/ImmunoSeq/AbVitro/flu_time_course/FV/single_reads_with_UMIs/FV_27.fastq  FLU_FV_27_${LOCI} $LOCI &
+    bash make_reference.sh /Engels/data/input/ImmunoSeq/Yale_UMI_Error_Correction/AAYHL_MG91M/AAYHL_MG91M-R12_primers-pass_pair-pass.fastq MG91M_${LOCI} $LOCI &
+    bash make_reference.sh /Engels/data/input/ImmunoSeq/Yale_UMI_Error_Correction/AAYHL_HD09M/AAYHL_HD09M-R12_primers-pass_pair-pass.fastq HD09M_${LOCI} $LOCI &
+    wait
+done
+
 exit
 
 for i in 1 2 3 4 5 6 7 8 9
@@ -14,7 +20,5 @@ do
 done
 wait
 
-bash make_reference.sh /Engels/data/input/ImmunoSeq/Yale_UMI_Error_Correction/AAYHL_MG91M/AAYHL_MG91M-R12_primers-pass_pair-pass.fastq MG91M &
-bash make_reference.sh /Engels/data/input/ImmunoSeq/Yale_UMI_Error_Correction/AAYHL_HD09M/AAYHL_HD09M-R12_primers-pass_pair-pass.fastq HD09M &
 wait
 
