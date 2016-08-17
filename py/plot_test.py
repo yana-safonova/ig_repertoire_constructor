f22e65c4da386ee2ac648f2144d682ea5979374d
Alexander Shlemov <shlemovalex@gmail.com>
2016-08-17 20:17:30 +0300

    wip

--- py/plot_test.py
+++ py/plot_test.py
@@ -44,91 +44,108 @@ def two_rocs(dir, tool1, tool2, out, label1=None, label2=None, title=""):
 def plotplot(dir, out_dir, title):
     mkdir_p(out_dir)
     rocs(dir,
-         tools=["igrec", "mixcr", "supernode"],
-         labels=["IgReC", "MiXCR", "pRESTO"],
+         tools=["igrec", "supernode"],
+         labels=["IgReC", "pRESTO"],
          title="Real data: " + title,
          out=out_dir + "/sensitivity_precision_plot_all")
     rocs(dir,
-         tools=["igrec_tau3", "mixcr", "supernode"],
-         labels=["IgReC tau = 3", "MiXCR", "pRESTO"],
+         tools=["igrec_tau3", "supernode"],
+         labels=["IgReC tau = 3", "pRESTO"],
          title="Real data: " + title,
          out=out_dir + "/sensitivity_precision_plot_all_tau3")
-
-    two_rocs(dir,
-             tool1="igrec", tool2="mixcr",
-             label1="IgReC",
-             label2="MiXCR",
-             title="Real data: " + title,
-             out=out_dir + "/sensitivity_precision_plot_igrec_mixcr")
-
-    two_rocs(dir,
-             tool1="igrec", tool2="supernode",
-             label1="IgReC",
-             label2="Supernode",
-             title="Real data: " + title,
-             out=out_dir + "/sensitivity_precision_plot_igrec_supernode")
-
-    two_rocs(dir,
-             tool1="igrec", tool2="igrec_tau1",
-             label1="IgReC",
-             label2="IgReC tau=1",
-             title="Real data: " + title,
-             out=out_dir + "/sensitivity_precision_plot_igrec_igrec_tau1")
-
-    two_rocs(dir,
-             tool1="igrec_tau1", tool2="supernode",
-             label1="IgReC tau=1",
-             label2="Supernode",
-             title="Real data: " + title,
-             out=out_dir + "/sensitivity_precision_plot_igrec_tau1_supernode")
-
-    two_rocs(dir,
-             tool1="mixcr", tool2="supernode",
-             label1="MiXCR",
-             label2="Supernode",
-             title="Real data: " + title,
-             out=out_dir + "/sensitivity_precision_plot_mixcr_supernode")
-
-    two_rocs(dir,
-             tool1="igrec_tau1_f09", tool2="supernode",
-             label1="IgReC tau=1 f09",
-             label2="Supernode",
-             title="Real data: " + title,
-             out=out_dir + "/sensitivity_precision_plot_igrec_tau1_f09_supernode")
-
-    two_rocs(dir,
-             tool1="igrec_tau2", tool2="supernode",
-             label1="IgReC tau=2",
-             label2="Supernode",
-             title="Real data: " + title,
-             out=out_dir + "/sensitivity_precision_plot_igrec_tau2_supernode")
-
-    two_rocs(dir,
-             tool1="igrec", tool2="igrec_tau2",
-             label1="IgReC",
-             label2="IgReC tau=2",
-             title="Real data: " + title,
-             out=out_dir + "/sensitivity_precision_plot_igrec_igrec_tau2")
-
-    two_rocs(dir,
-             tool1="igrec", tool2="igrec_tau3",
-             label1="IgReC",
-             label2="IgReC tau=3",
-             title="Real data: " + title,
-             out=out_dir + "/sensitivity_precision_plot_igrec_igrec_tau3")
+    rocs(dir,
+         tools=["igrec_tau2", "supernode"],
+         labels=["IgReC tau = 2", "pRESTO"],
+         title="Real data: " + title,
+         out=out_dir + "/sensitivity_precision_plot_all_tau2")
+    rocs(dir,
+         tools=["igrec_tau2", "supernode"],
+         labels=["IgReC tau = 1", "pRESTO"],
+         title="Real data: " + title,
+         out=out_dir + "/sensitivity_precision_plot_all_tau1")
+    #
+    # two_rocs(dir,
+    #          tool1="igrec", tool2="mixcr",
+    #          label1="IgReC",
+    #          label2="MiXCR",
+    #          title="Real data: " + title,
+    #          out=out_dir + "/sensitivity_precision_plot_igrec_mixcr")
+    #
+    # two_rocs(dir,
+    #          tool1="igrec", tool2="supernode",
+    #          label1="IgReC",
+    #          label2="Supernode",
+    #          title="Real data: " + title,
+    #          out=out_dir + "/sensitivity_precision_plot_igrec_supernode")
+    #
+    # two_rocs(dir,
+    #          tool1="igrec", tool2="igrec_tau1",
+    #          label1="IgReC",
+    #          label2="IgReC tau=1",
+    #          title="Real data: " + title,
+    #          out=out_dir + "/sensitivity_precision_plot_igrec_igrec_tau1")
+    #
+    # two_rocs(dir,
+    #          tool1="igrec_tau1", tool2="supernode",
+    #          label1="IgReC tau=1",
+    #          label2="Supernode",
+    #          title="Real data: " + title,
+    #          out=out_dir + "/sensitivity_precision_plot_igrec_tau1_supernode")
+    #
+    # two_rocs(dir,
+    #          tool1="mixcr", tool2="supernode",
+    #          label1="MiXCR",
+    #          label2="Supernode",
+    #          title="Real data: " + title,
+    #          out=out_dir + "/sensitivity_precision_plot_mixcr_supernode")
+    #
+    # two_rocs(dir,
+    #          tool1="igrec_tau1_f09", tool2="supernode",
+    #          label1="IgReC tau=1 f09",
+    #          label2="Supernode",
+    #          title="Real data: " + title,
+    #          out=out_dir + "/sensitivity_precision_plot_igrec_tau1_f09_supernode")
+    #
+    # two_rocs(dir,
+    #          tool1="igrec_tau2", tool2="supernode",
+    #          label1="IgReC tau=2",
+    #          label2="Supernode",
+    #          title="Real data: " + title,
+    #          out=out_dir + "/sensitivity_precision_plot_igrec_tau2_supernode")
+    #
+    # two_rocs(dir,
+    #          tool1="igrec", tool2="igrec_tau2",
+    #          label1="IgReC",
+    #          label2="IgReC tau=2",
+    #          title="Real data: " + title,
+    #          out=out_dir + "/sensitivity_precision_plot_igrec_igrec_tau2")
+    #
+    # two_rocs(dir,
+    #          tool1="igrec", tool2="igrec_tau3",
+    #          label1="IgReC",
+    #          label2="IgReC tau=3",
+    #          title="Real data: " + title,
+    #          out=out_dir + "/sensitivity_precision_plot_igrec_igrec_tau3")
 
 if __name__ == "__main__":
     plotplot(igrec_dir + "/src/extra/ig_quast_tool/FLU_FV_21_IGH/filtering1/", "FLU_FV_21_IGH_1", title="FLU_FV_21_IGH_1")
-    plotplot(igrec_dir + "/src/extra/ig_quast_tool/FLU_FV_21_IGH/filtering2/", "FLU_FV_21_IGH_2", title="FLU_FV_21_IGH_2")
-    plotplot(igrec_dir + "/src/extra/ig_quast_tool/FLU_FV_21_IGH/filtering3/", "FLU_FV_21_IGH_3", title="FLU_FV_21_IGH_3")
+    # plotplot(igrec_dir + "/src/extra/ig_quast_tool/FLU_FV_21_IGH/filtering2/", "FLU_FV_21_IGH_2", title="FLU_FV_21_IGH_2")
+    # plotplot(igrec_dir + "/src/extra/ig_quast_tool/FLU_FV_21_IGH/filtering3/", "FLU_FV_21_IGH_3", title="FLU_FV_21_IGH_3")
 
     plotplot(igrec_dir + "/src/extra/ig_quast_tool/FLU_FV_21_IGL/filtering1/", "FLU_FV_21_IGL_1", title="FLU_FV_21_IGL_1")
-    plotplot(igrec_dir + "/src/extra/ig_quast_tool/FLU_FV_21_IGL/filtering2/", "FLU_FV_21_IGL_2", title="FLU_FV_21_IGL_2")
-    plotplot(igrec_dir + "/src/extra/ig_quast_tool/FLU_FV_21_IGL/filtering3/", "FLU_FV_21_IGL_3", title="FLU_FV_21_IGL_3")
+    # plotplot(igrec_dir + "/src/extra/ig_quast_tool/FLU_FV_21_IGL/filtering2/", "FLU_FV_21_IGL_2", title="FLU_FV_21_IGL_2")
+    # plotplot(igrec_dir + "/src/extra/ig_quast_tool/FLU_FV_21_IGL/filtering3/", "FLU_FV_21_IGL_3", title="FLU_FV_21_IGL_3")
 
-    plotplot(igrec_dir + "/src/extra/ig_quast_tool/FLU_FV_21_IGK/filtering1/", "FLU_FV_21_IGK_1", title="FLU_FV_21_IGK_1")
-    plotplot(igrec_dir + "/src/extra/ig_quast_tool/FLU_FV_21_IGK/filtering2/", "FLU_FV_21_IGK_2", title="FLU_FV_21_IGK_2")
-    plotplot(igrec_dir + "/src/extra/ig_quast_tool/FLU_FV_21_IGK/filtering3/", "FLU_FV_21_IGK_3", title="FLU_FV_21_IGK_3")
+    # plotplot(igrec_dir + "/src/extra/ig_quast_tool/FLU_FV_21_IGK/filtering1/", "FLU_FV_21_IGK_1", title="FLU_FV_21_IGK_1")
+    # plotplot(igrec_dir + "/src/extra/ig_quast_tool/FLU_FV_21_IGK/filtering2/", "FLU_FV_21_IGK_2", title="FLU_FV_21_IGK_2")
+    # plotplot(igrec_dir + "/src/extra/ig_quast_tool/FLU_FV_21_IGK/filtering3/", "FLU_FV_21_IGK_3", title="FLU_FV_21_IGK_3")
+
+    plotplot(igrec_dir + "/src/extra/ig_quast_tool/FLU_FV_21_IGH/filtering1_jit0.5/", "FLU_FV_21_IGH_1_jit0.5", title="FLU_FV_21_IGH_1_jit0.5")
+    plotplot(igrec_dir + "/src/extra/ig_quast_tool/FLU_FV_21_IGH/filtering1_jit1/", "FLU_FV_21_IGH_1_jit1", title="FLU_FV_21_IGH_1_jit1")
+    plotplot(igrec_dir + "/src/extra/ig_quast_tool/FLU_FV_21_IGL/filtering1_jit0.5/", "FLU_FV_21_IGL_1_jit0.5", title="FLU_FV_21_IGL_1_jit0.5")
+    plotplot(igrec_dir + "/src/extra/ig_quast_tool/FLU_FV_21_IGL/filtering1_jit1/", "FLU_FV_21_IGL_1_jit1", title="FLU_FV_21_IGL_1_jit1")
+
+    sys.exit()
 
     plotplot(igrec_dir + "/src/extra/ig_quast_tool/AGE3/filtering3/", "AGE3_3", title="AGE3")
     plotplot(igrec_dir + "/src/extra/ig_quast_tool/AGE3/filtering2/", "AGE3_2", title="AGE3")
--- py/test_on_barcoded_datasets.py
+++ py/test_on_barcoded_datasets.py
@@ -4,18 +4,19 @@ from test_on_naive_simulation import *
 
 
 def run_and_quast_all_three(input_dir, threads=16):
-    for i in [1, 2, 3]:
+    # for i in [1, 2, 3]:
+    for i in [3]:
         run_and_quast_all(input_dir + "/input%d.fa.gz" % i,
                           input_dir + "/repertoire%d.fa.gz" % i,
                           input_dir + "/repertoire%d.rcm" % i,
                           out_dir=input_dir + "/filtering%d" % i,
                           threads=threads)
-        for lam in ["0.5", "1"]:
-            run_and_quast_all(input_dir + "/input%d_jit%s.fa.gz" % (i, lam),
-                              input_dir + "/repertoire%d.fa.gz" % i,
-                              input_dir + "/repertoire%d.rcm" % i,
-                              out_dir=input_dir + "/filtering%d_jit%s" % (i, lam),
-                              threads=threads)
+        # for lam in ["0.5", "1"]:
+        #     run_and_quast_all(input_dir + "/input%d_jit%s.fa.gz" % (i, lam),
+        #                       input_dir + "/repertoire%d.fa.gz" % i,
+        #                       input_dir + "/repertoire%d.rcm" % i,
+        #                       out_dir=input_dir + "/filtering%d_jit%s" % (i, lam),
+        #                       threads=threads)
 
 
 if __name__ == "__main__":
@@ -31,6 +32,7 @@ if __name__ == "__main__":
     datasets = ["AGE3", "AGE7", "MG91M_IGH", "MG91M_IGL", "MG91M_IGK", "FLU_FV_21_IGH", "FLU_FV_21_IGL", "FLU_FL_21_IGK", "FLU_FV_27_IGH", "FLU_FV_27_IGK", "FLU_FV_27_IGL"]
     datasets = ["AGE7", "MG91M_IGH", "MG91M_IGL", "MG91M_IGK", "FLU_FV_21_IGH", "FLU_FV_21_IGL", "FLU_FL_21_IGK", "FLU_FV_27_IGH", "FLU_FV_27_IGK", "FLU_FV_27_IGL"]
     datasets = ["FLU_FV_21_IGH", "FLU_FV_21_IGL", "FLU_FL_21_IGK", "FLU_FV_27_IGH", "FLU_FV_27_IGK", "FLU_FV_27_IGL"]
+    datasets = ["MG91M_IGH", "MG91M_IGL", "MG91M_IGK", "HD09M_IGH", "HD09M_IGL", "HD09M_IGK"]
 
     for dataset in datasets:
         ddir = igrec_dir + "/src/extra/ig_quast_tool/" + dataset
