945e06c2d5724089bd783a192868668710bf0bae
Alexander Shlemov <shlemovalex@gmail.com>
2016-08-22 13:17:38 +0300

    Update sim script

--- py/test_on_naive_simulation.py
+++ py/test_on_naive_simulation.py
@@ -46,37 +46,42 @@ def run_and_quast_all(input_reads,
                       output_dir=out_dir + "/" + self.name + "/")
 
     igrec_runs = []
-    igrec_runs.append(IgReCRun("igrec_trivial", trivial=True))
+    # igrec_runs.append(IgReCRun("igrec_trivial", trivial=True))
     # igrec_runs.append(IgReCRun("igrec_trivial_tau3", tau=3, trivial=True))
     # igrec_runs.append(IgReCRun("igrec_trivial_tau2", tau=2, trivial=True))
     # igrec_runs.append(IgReCRun("igrec_trivial_tau1", tau=1, trivial=True))
 
     igrec_runs.append(IgReCRun("igrec"))
-    igrec_runs.append(IgReCRun("igrec_msns2", min_sread_size=2))
-    igrec_runs.append(IgReCRun("igrec_msns3", min_sread_size=3))
-    igrec_runs.append(IgReCRun("igrec_msns4", min_sread_size=4))
-    # igrec_runs.append(IgReCRun("igrec_f03", fillin=0.3))
-    # igrec_runs.append(IgReCRun("igrec_f075", fillin=0.75))
-    # igrec_runs.append(IgReCRun("igrec_f09", fillin=0.9))
-    # igrec_runs.append(IgReCRun("igrec_f095", fillin=0.95))
     igrec_runs.append(IgReCRun("igrec_tau3", tau=3))
-    igrec_runs.append(IgReCRun("igrec_tau3_msns3", tau=3, min_sread_size=3))
-    igrec_runs.append(IgReCRun("igrec_tau3_f03", tau=3, fillin=0.3))
-    # igrec_runs.append(IgReCRun("igrec_tau3_f075", tau=3, fillin=0.75))
-    # igrec_runs.append(IgReCRun("igrec_tau3_f09", tau=3, fillin=0.9))
-    igrec_runs.append(IgReCRun("igrec_tau3_f095", tau=3, fillin=0.95))
+    igrec_runs.append(IgReCRun("igrec_tau2", tau=2))
+
+    igrec_runs.append(IgReCRun("igrec_msns2", min_sread_size=2))
+    igrec_runs.append(IgReCRun("igrec_tau3_msns2", tau=3, min_sread_size=2))
     igrec_runs.append(IgReCRun("igrec_tau2_msns2", tau=2, min_sread_size=2))
-    # igrec_runs.append(IgReCRun("igrec_tau2", tau=2))
-    # igrec_runs.append(IgReCRun("igrec_tau2_f03", tau=2, fillin=0.3))
-    # igrec_runs.append(IgReCRun("igrec_tau2_f075", tau=2, fillin=0.75))
-    # igrec_runs.append(IgReCRun("igrec_tau2_f09", tau=2, fillin=0.9))
-    igrec_runs.append(IgReCRun("igrec_tau2_f095", tau=2, fillin=0.95))
-    igrec_runs.append(IgReCRun("igrec_tau2_f099", tau=2, fillin=0.99))
-    # igrec_runs.append(IgReCRun("igrec_tau1", tau=1))
-    # igrec_runs.append(IgReCRun("igrec_tau1_f03", tau=1, fillin=0.3))
-    # igrec_runs.append(IgReCRun("igrec_tau1_f075", tau=1, fillin=0.75))
-    # igrec_runs.append(IgReCRun("igrec_tau1_f09", tau=1, fillin=0.9))
-    # igrec_runs.append(IgReCRun("igrec_tau1_f095", tau=1, fillin=0.95))
+    # # igrec_runs.append(IgReCRun("igrec_msns3", min_sread_size=3))
+    # # igrec_runs.append(IgReCRun("igrec_msns4", min_sread_size=4))
+    # # igrec_runs.append(IgReCRun("igrec_f03", fillin=0.3))
+    # # igrec_runs.append(IgReCRun("igrec_f075", fillin=0.75))
+    # # igrec_runs.append(IgReCRun("igrec_f09", fillin=0.9))
+    # # igrec_runs.append(IgReCRun("igrec_f095", fillin=0.95))
+    # igrec_runs.append(IgReCRun("igrec_tau3", tau=3))
+    # igrec_runs.append(IgReCRun("igrec_tau3_msns3", tau=3, min_sread_size=3))
+    # igrec_runs.append(IgReCRun("igrec_tau3_f03", tau=3, fillin=0.3))
+    # # igrec_runs.append(IgReCRun("igrec_tau3_f075", tau=3, fillin=0.75))
+    # # igrec_runs.append(IgReCRun("igrec_tau3_f09", tau=3, fillin=0.9))
+    # igrec_runs.append(IgReCRun("igrec_tau3_f095", tau=3, fillin=0.95))
+    # igrec_runs.append(IgReCRun("igrec_tau2_msns2", tau=2, min_sread_size=2))
+    # # igrec_runs.append(IgReCRun("igrec_tau2", tau=2))
+    # # igrec_runs.append(IgReCRun("igrec_tau2_f03", tau=2, fillin=0.3))
+    # # igrec_runs.append(IgReCRun("igrec_tau2_f075", tau=2, fillin=0.75))
+    # # igrec_runs.append(IgReCRun("igrec_tau2_f09", tau=2, fillin=0.9))
+    # igrec_runs.append(IgReCRun("igrec_tau2_f095", tau=2, fillin=0.95))
+    # igrec_runs.append(IgReCRun("igrec_tau2_f099", tau=2, fillin=0.99))
+    # # igrec_runs.append(IgReCRun("igrec_tau1", tau=1))
+    # # igrec_runs.append(IgReCRun("igrec_tau1_f03", tau=1, fillin=0.3))
+    # # igrec_runs.append(IgReCRun("igrec_tau1_f075", tau=1, fillin=0.75))
+    # # igrec_runs.append(IgReCRun("igrec_tau1_f09", tau=1, fillin=0.9))
+    # # igrec_runs.append(IgReCRun("igrec_tau1_f095", tau=1, fillin=0.95))
 
     if not do_not_run:
         for run in igrec_runs:
@@ -88,6 +93,7 @@ def run_and_quast_all(input_reads,
         run_presto(input_reads, output_dir=out_dir + "/presto/")
 
         mkdir_p(out_dir + "/supernode")
+        # TODO use runs instead of hardcoded igrec
         shutil.copy(out_dir + "/igrec/supernode_repertoire.fa",
                     out_dir + "/supernode/final_repertoire.fa")
         shutil.copy(out_dir + "/igrec/supernode_repertoire.rcm",
@@ -128,6 +134,7 @@ if __name__ == "__main__":
     output_dirs = [igrec_dir + "/various_error_rate", igrec_dir + "/var_err_rate_real"]
 
     lambdas = [0, 0.0625, 0.125, 0.25, 0.375, 0.5, 0.75, 1, 1.25, 1.5, 1.75, 2, 2.5, 3, 3.5, 4]
+    lambdas = [0, 0.0625, 0.125, 0.25, 0.375, 0.5, 0.75, 1, 1.25, 1.5, 1.75, 2, 2.5]
     for dataset, output_dir in reversed(zip(datasets, output_dirs)):
         if not os.path.isfile(dataset):
             continue
