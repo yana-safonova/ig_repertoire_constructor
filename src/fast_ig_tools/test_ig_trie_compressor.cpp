9574c31166e0c21cdf162d972be000528838e6ba
Alexander Shlemov <shlemovalex@gmail.com>
2016-09-08 02:04:34 +0300

    TrieComp: Add the first test

---
+++ src/fast_ig_tools/test_ig_trie_compressor.cpp
@@ -0,0 +1,30 @@
+#include <gmock/gmock.h>
+
+#include "ig_trie_compressor.hpp"
+
+using namespace fast_ig_tools;
+using namespace ::testing;
+
+TEST(basic_tests, the_first_test) {
+    std::vector<std::string> reads = {"AAA", "AAAA", "AAAC"};
+
+    auto indices = compressed_reads_indices(reads);
+    auto comp_reads = compressed_reads(reads.begin(), reads.end());
+
+    EXPECT_THAT(indices.size(), reads.size());
+    EXPECT_THAT(indices, ElementsAre(0, 0, 0));
+    EXPECT_THAT(comp_reads.size(), 1);
+    EXPECT_THAT(comp_reads, ElementsAre("AAA"));
+}
+
+TEST(basic_tests, empty_string_test) {
+    std::vector<std::string> reads = {"AAA", "AAAA", "AAAC", "", "XXX", "sdadasdasd"};
+
+    auto indices = compressed_reads_indices(reads);
+    auto comp_reads = compressed_reads(reads);
+
+    EXPECT_THAT(indices.size(), reads.size());
+    EXPECT_THAT(indices, ElementsAre(3, 3, 3, 3, 3, 3));
+    EXPECT_THAT(comp_reads.size(), 1);
+    EXPECT_THAT(comp_reads, ElementsAre(""));
+}
