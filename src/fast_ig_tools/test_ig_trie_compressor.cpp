e46e14dec2cc301b05ecf5822f248585232e0d07
Alexander Shlemov <shlemovalex@gmail.com>
2016-09-08 02:37:34 +0300

    TrieComp: Add test

--- src/fast_ig_tools/test_ig_trie_compressor.cpp
+++ src/fast_ig_tools/test_ig_trie_compressor.cpp
@@ -28,3 +28,15 @@ TEST(basic_tests, empty_string_test) {
     EXPECT_THAT(comp_reads.size(), 1);
     EXPECT_THAT(comp_reads, ElementsAre(""));
 }
+
+TEST(basic_tests, two_clusters) {
+    std::vector<std::string> reads = {"AAA", "AAAA", "AAAC", "XX", "XXAA"};
+
+    auto indices = compressed_reads_indices(reads);
+    auto comp_reads = compressed_reads(reads);
+
+    EXPECT_THAT(indices.size(), reads.size());
+    EXPECT_THAT(indices, ElementsAre(0, 0, 0, 3, 3));
+    EXPECT_THAT(comp_reads.size(), 2);
+    EXPECT_THAT(comp_reads, ElementsAre("AAA", "XX"));
+}
