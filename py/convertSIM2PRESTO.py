212a5c227588ac494e029ed615558a152908811c
Sergey Bankevich <Sergey.Bankevich@gmail.com>
2016-10-02 23:04:17 +0300

    tools

--- py/convertSIM2PRESTO.py
+++ py/convertSIM2PRESTO.py
@@ -8,37 +8,30 @@ from argparse import ArgumentParser
 from simulate import *
 
 
-def convert_id(id, primer="NA"):
+def convert_id(id, index):
     import re
-    m = re.match(r"^(.*)UMI:([ACGTN]+):?(.*)$", id)
+    m = re.match(r"^.*DUPCOUNT=(\d+)$", id)
 
     if m:
         g = m.groups()
-        id = g[0].strip()
-        barcode = g[1].strip()
-        return id + "|PRIMER=%s|BARCODE=%s" % (primer, barcode)
+        id = "cluster___%d___size___%d" % (index, int(g[0].strip()))
+        return id
     else:
         return None
 
-assert convert_id("M01691:10:000000000-A7F7L:1:1101:18193:1582_1:N:0:1_UMI:GCGGAATATTTCA:DF77+,E<FEGGF") == \
-        "M01691:10:000000000-A7F7L:1:1101:18193:1582_1:N:0:1_|PRIMER=NA|BARCODE=GCGGAATATTTCA"
+assert convert_id("", 2345) is None
 
-assert convert_id("34014_mutated_from_4083_UMI:TTTGTTCAATCTCG") == \
-        "34014_mutated_from_4083_|PRIMER=NA|BARCODE=TTTGTTCAATCTCG"
-
+assert convert_id(">1TTTTCAATCGACGGA|PRCONS=NA|PRFREQ=1.0|CONSCOUNT=30|DUPCOUNT=3", 2345) == \
+        "cluster___2345___size___3"
 
 if __name__ == "__main__":
-    parser = ArgumentParser(description="Convert AGE dataset to PRESTO")
+    parser = ArgumentParser(description="Convert pRESTO output repertoire to quast format")
     parser.add_argument("input",
                         type=str,
                         help="input FASTA/FASTQ file")
     parser.add_argument("output",
                         type=str,
                         help="output FASTA/FASTQ file")
-    parser.add_argument("--primer", "-p",
-                        type=str,
-                        default="NA",
-                        help="primer name to add")
 
     args = parser.parse_args()
     print "Command line: %s" % " ".join(sys.argv)
@@ -46,9 +39,11 @@ if __name__ == "__main__":
     input_format = idFormatByFileName(args.input)
     output_format = idFormatByFileName(args.output)
 
+    current = 0
     with smart_open(args.input) as fh, smart_open(args.output, "w") as fout:
         for record in SeqIO.parse(fh, input_format):
-            record.id = record.description = convert_id(record.description, args.primer)
+            record.id = record.description = convert_id(record.description, current)
             SeqIO.write(record, fout, output_format)
+            current += 1
 
     print "Conversion %s -> %s done" % (args.input, args.output)
