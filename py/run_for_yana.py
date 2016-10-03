5042cef6b00ead218ef8c8c64d5a88030da0499e
Alexander Shlemov <shlemovalex@gmail.com>
2016-10-04 00:02:46 +0300

    Add processing for the paper

---
+++ py/run_for_yana.py
@@ -0,0 +1,13 @@
+from simulate import *
+
+if __name__ == "__main__":
+    data1 = "/Sid/ysafonova/ig_repertoire_constructor/BSSE_naive/vj_finder/cleaned_reads.fa"
+    data2 = "/Sid/ysafonova/ig_repertoire_constructor/BSSE_asc/vj_finder/cleaned_reads.fa"
+    data3 = "/Sid/ysafonova/ig_repertoire_constructor/BSSE_plasma/vj_finder/cleaned_reads.fa"
+
+    datas = [data1, data2, data3]
+    names = ["naive", "asc", "plasma"]
+
+    for data, name in zip(datas, names):
+        run_mixcr(data, "mice_out_mixcr_" + name, species="mmu")
+        run_presto(data, "mice_out_presto_" + name)
