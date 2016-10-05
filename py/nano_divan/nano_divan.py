450d5c01313b262cbf08d58a58003b871ae031bb
Alexander Shlemov <shlemovalex@gmail.com>
2016-10-05 11:31:56 +0300

    Nanodivan uses SWG

--- py/nano_divan/nano_divan.py
+++ py/nano_divan/nano_divan.py
@@ -5,7 +5,6 @@ from Bio import SeqIO
 import sys
 import os
 import os.path
-import tempfile
 from collections import defaultdict
 
 import numpy as np
@@ -70,6 +69,58 @@ def hamming(s1, s2):
 
 import itertools
 
+
+def write_cdr3s(cdr3s, filename):
+    with open(filename, "w") as f:
+        for i, s in enumerate(cdr3s):
+            f.write(">id%d\n%s\n" % (i, s))
+
+
+current_dir = os.path.dirname(os.path.realpath(__file__))
+igrec_dir = current_dir + "/../../"
+
+
+def call_swg(inputfile, outputfile):
+    run_graph_constructor = igrec_dir + "/build/release/bin/ig_swgraph_construct"
+    cmd = run_graph_constructor + " -i " + inputfile + " -o " + outputfile + " --tau 3 -T 0 -S1 -k4"
+    os.system(cmd)
+
+
+def parse_conn_comp_ids(graph_file):
+    with open(graph_file) as g:
+        header = next(g)
+        V, E, _ = header.strip().split()
+        V = int(V)
+        E = int(E)
+
+        dsu = DSU(V)
+
+        for i, line in enumerate(g):
+            line = line.strip().split()
+            neibs = line[0::2]
+            neibs = map(int, neibs)
+            neibs = [j - 1 for j in neibs]
+            for j in neibs:
+                dsu.join(i, j)
+
+    return dsu.getAll()
+
+
+def conn_comp(cdrs):
+    import tempfile
+
+    CDR_fa = tempfile.mkstemp(suffix=".fa", prefix="nanodivan_")[1]
+    CDR_graph = tempfile.mkstemp(suffix=".graph", prefix="nanodivan_")[1]
+    write_cdr3s(cdrs, CDR_fa)
+    call_swg(CDR_fa, CDR_graph)
+    ids = parse_conn_comp_ids(CDR_graph)
+
+    os.remove(CDR_fa)
+    os.remove(CDR_graph)
+
+    return ids
+
+
 if __name__ == "__main__":
     filename = "/ssd/ig_repertoire_constructor/py/nano_divan/test7_mixcr2/features.txt"
     filename = sys.argv[1]
@@ -82,10 +133,12 @@ if __name__ == "__main__":
     for cdr3 in CDR3s:
         abundances[cdr3] += 1
 
+    unique_cdr3s = abundances.keys()
+    abundances = [abundances[key] for key in unique_cdr3s]
     print "# unique CDR3", len(abundances)
-    print "Max CDR3 abundance", max(abundances.values())
+    print "Max CDR3 abundance", max(abundances)
 
-    Ns = np.array(abundances.values(), dtype=float)
+    Ns = np.array(abundances, dtype=float)
     freqs = Ns / sum(Ns)
 
     shannon_index = -sum(freqs * np.log(freqs))
@@ -94,18 +147,23 @@ if __name__ == "__main__":
     print "Shannon:", shannon_index
     print "Simpson:", simpson_index
 
-    unique_cdr3s = abundances.keys()
-    dsu = DSU(len(unique_cdr3s))
-
-    tau = 3
-    for i, j in itertools.combinations(range(len(unique_cdr3s)), 2):
-        if hamming(unique_cdr3s[i], unique_cdr3s[j]) <= tau:
-            dsu.join(i, j)
+    # dsu = DSU(len(unique_cdr3s))
+    #
+    #
+    # write_cdr3s(unique_cdr3s, "CDR.fa")
+    # call_swg("CDR.fa", "CDR.graph")
+    #
+    # tau = 3
+    # for i, j in itertools.combinations(range(len(unique_cdr3s)), 2):
+    #     if hamming(unique_cdr3s[i], unique_cdr3s[j]) <= tau:
+    #         dsu.join(i, j)
+    # labels = dsu.getAll()
+    #
+    labels = conn_comp(unique_cdr3s)
 
     compsizecdrs = defaultdict(int)
     compsizeuniquecdrs = defaultdict(int)
 
-    labels = dsu.getAll()
     for label, size in zip(labels, Ns):  # TODO check consistensy keys-values
         compsizecdrs[label] += size
         compsizeuniquecdrs[label] += 1
