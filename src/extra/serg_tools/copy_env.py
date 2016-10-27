40b4d67d6933f65d6fed27ace8da2cf67bfb9553
Sergey Bankevich <Sergey.Bankevich@gmail.com>
2016-10-28 00:16:14 +0300

    script for code version isolation

---
+++ src/extra/serg_tools/copy_env.py
@@ -0,0 +1,31 @@
+import fnmatch
+import os
+import shutil
+import sys
+
+
+def make_dir_if_not_there(dest):
+    try:
+        os.makedirs(dest)
+    except OSError:
+        if not os.path.isdir(dest):
+            raise
+
+
+def main():
+    dest = sys.argv[1]
+    shutil.rmtree(dest, ignore_errors = True)
+    os.makedirs(dest)
+    current_dir = os.path.dirname(os.path.realpath(__file__))
+    igrec_dir = os.path.join(current_dir, os.pardir, os.pardir, os.pardir)
+    for root, dirs, files in os.walk(igrec_dir):
+        for file in fnmatch.filter(files, "*.py"):
+            dir = os.path.relpath(root, igrec_dir)
+            dest_dir = os.path.join(dest, dir)
+            make_dir_if_not_there(dest_dir)
+            shutil.copy2(os.path.join(root, file), dest_dir)
+    for dir in ["build", "pipeline_makefiles", "configs", "data"]:
+        shutil.copytree(os.path.join(igrec_dir, dir), os.path.join(dest, dir))
+
+if __name__ == '__main__':
+    main()
