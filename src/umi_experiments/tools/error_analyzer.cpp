17ff65fe5284e49742eecf3e90b68f2aa9f4f92e
Sergey Bankevich <Sergey.Bankevich@gmail.com>
2016-11-11 18:52:03 +0300

    minor fix

--- src/umi_experiments/tools/error_analyzer.cpp
+++ src/umi_experiments/tools/error_analyzer.cpp
@@ -22,13 +22,14 @@ void ErrorAnalyzer::performAnalysis() {
         for (size_t idx : read_idx_list) {
             read_to_count.insert(seqan_string_to_string(reads_[idx]));
         }
-        std::vector<size_t> sizes(read_to_count.size());
-        std::transform(read_to_count.begin(), read_to_count.end(), sizes.begin(), [&read_to_count](std::string read) { return read_to_count.count(read); });
+        std::unordered_set<std::string> read_set(read_to_count.begin(), read_to_count.end());
+        std::vector<size_t> sizes(read_set.size());
+        std::transform(read_set.begin(), read_set.end(), sizes.begin(), [&read_to_count](std::string read) { return read_to_count.count(read); });
         std::sort(sizes.begin(), sizes.end(), std::greater<size_t>());
-        VERIFY(!sizes.empty());
+        VERIFY_MSG(!sizes.empty(), "Empty group unexpected.");
         size_t sum = std::accumulate(sizes.begin(), sizes.end(), (size_t) 0);
-        VERIFY(read_idx_list.size() == sum);
-        if (sizes[0] < 5 || sizes[0] * 10 < sum) continue;
+        VERIFY_MSG(read_idx_list.size() == sum, boost::format("Read group sizes sum to %1% while there were %2% reads sharing barcode.") % sum % read_idx_list.size());
+        if (sizes[0] < 5 || sizes[0] * 10 < sum || (sizes.size() >= 2 && sizes[1] * 3 > sizes[0] * 2)) continue;
         std::stringstream sstr;
         for (size_t size : sizes) {
             sstr << size << ", ";
--- src/umi_experiments/tools/report_pcr_error_rate.cpp
+++ src/umi_experiments/tools/report_pcr_error_rate.cpp
@@ -6,7 +6,7 @@
 
 void create_console_logger() {
     using namespace logging;
-    logger *lg = create_logger("report_pcr_error_rate");
+    logger *lg = create_logger("");
     lg->add_writer(std::make_shared<console_writer>());
     attach_logger(lg);
 }
