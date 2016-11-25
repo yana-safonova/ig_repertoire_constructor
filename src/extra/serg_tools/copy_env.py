efd32aa9ff0a210038397543aab4e60651bb282a
Sergey Bankevich <Sergey.Bankevich@gmail.com>
2016-11-25 20:43:08 +0300

    TC artifacts script

---
+++ src/extra/serg_tools/copy_env.py
@@ -0,0 +1,35 @@
+import fnmatch
+import os
+import shutil
+import sys
+
+
+def make_dir_if_not_there(dest):
+    try:
+        os.makedirs(dest)
+    except OSError:
+        if not os.path.isdir(dest):
+            raise
+
+
+def main():
+    dest = sys.argv[1]
+    clean = int(sys.argv[2]) > 0 if len(sys.argv) > 2 else True
+    if clean:
+        shutil.rmtree(dest, ignore_errors = True)
+    make_dir_if_not_there(dest)
+    current_dir = os.path.dirname(os.path.realpath(__file__))
+    igrec_dir = os.path.join(current_dir, os.pardir, os.pardir, os.pardir)
+    for root, dirs, files in os.walk(igrec_dir):
+        for file in [file for file in files if file.endswith((".py", ".jar", ".sh"))]:
+            dir = os.path.relpath(root, igrec_dir)
+            dest_dir = os.path.join(dest, dir)
+            make_dir_if_not_there(dest_dir)
+            shutil.copy2(os.path.join(root, file), dest_dir)
+    for dir in ["build", "pipeline_makefiles", "configs", "data"]:
+        shutil.copytree(os.path.join(igrec_dir, dir), os.path.join(dest, dir))
+
+    os.system("git -C %s log | head > %s/GIT_REVISION" % (igrec_dir, dest))
+
+if __name__ == '__main__':
+    main()
