031fd1f98b7dd021cd02c7e6ea025dac71e2f531
Sergey Bankevich <Sergey.Bankevich@gmail.com>
2016-12-02 18:34:03 +0300

    fix git rev

--- teamcity/build.sh
+++ teamcity/build.sh
@@ -1,7 +1,8 @@
+#!/usr/bin/env bash
 echo "building"
 ./prepare_cfg
 make -j8
 
 echo "exporting artifacts"
 python ./src/extra/serg_tools/copy_env.py env 1
-echo %build.vcs.number% > env/GIT_REVISION
+echo $1 > env/GIT_REVISION
