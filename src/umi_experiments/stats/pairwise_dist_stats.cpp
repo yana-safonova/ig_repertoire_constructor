74f6c9fee4299d9551eed32d70199162adfc8ddf
Sergey Bankevich <Sergey.Bankevich@gmail.com>
2016-12-19 19:41:14 +0300

    IG-17 reduce file size for dists between clusters

--- src/umi_experiments/stats/pairwise_dist_stats.cpp
+++ src/umi_experiments/stats/pairwise_dist_stats.cpp
@@ -7,30 +7,33 @@ void calc_and_write(int threads, const std::vector<seqan::Dna5String>& reads, si
     omp_set_num_threads(threads);
 
     const size_t size = reads.size();
+    std::vector<size_t> dists(max_dist + 2);
     const auto& get_dist = clusterer::ClusteringMode::bounded_edit_dist(max_dist, max_dist, false);
     SEQAN_OMP_PRAGMA(parallel for schedule(dynamic, 8))
     for (size_t i = 0; i < size; i ++) {
-        std::vector<size_t> dist(size - i - 1);
+        std::vector<size_t> local_dists(max_dist + 2);
 
         for (size_t j = i + 1; j < size; j ++) {
-            dist[j - i - 1] = get_dist(reads[i], reads[j]);
+            local_dists[get_dist(reads[i], reads[j])] ++;
         }
         SEQAN_OMP_PRAGMA(critical)
-        {
-            std::for_each(dist.begin(), dist.end(), [&ofs, &max_dist](size_t d) { if (d <= max_dist) ofs << d << "\n"; });
+        for (size_t d = 0; d <= max_dist + 1; d ++) {
+            dists[d] += local_dists[d];
         }
         std::cout << i << "\n" << clock() << std::endl;
     }
+    std::for_each(dists.begin(), dists.end(), [&ofs](size_t d) { ofs << d << "\n"; });
 }
 
-int main(int argc, const char* const* argv) {
+int main(int, const char* const* argv) {
     perf_counter pc;
     segfault_handler sh;
+    create_console_logger();
 
-    const std::string repertoire_file(argv[1]);
+    const std::string reads_file(argv[1]);
     std::vector<seqan::CharString> ids;
     std::vector<seqan::Dna5String> reads;
-    read_seqan_records(repertoire_file, ids, reads);
+    read_seqan_records(reads_file, ids, reads);
 
     const std::string dist_output_file(argv[2]);
     std::ofstream ofs(dist_output_file);
