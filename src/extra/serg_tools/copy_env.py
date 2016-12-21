21e6bf69d278699d54f18af0e28a77c11c1985bf
Sergey Bankevich <Sergey.Bankevich@gmail.com>
2016-12-21 20:32:36 +0300

    include test ds to copied env

--- src/extra/serg_tools/copy_env.py
+++ src/extra/serg_tools/copy_env.py
@@ -12,6 +12,14 @@ def make_dir_if_not_there(dest):
             raise
 
 
+def copy_file(dest, file, igrec_dir, root):
+    dir = os.path.relpath(root, igrec_dir)
+    dest_dir = os.path.join(dest, dir)
+    make_dir_if_not_there(dest_dir)
+    # print "copying %s to %s" % (os.path.join(root, file), dest_dir)
+    shutil.copy2(os.path.join(root, file), dest_dir)
+
+
 def main():
     dest = sys.argv[1]
     clean = int(sys.argv[2]) > 0 if len(sys.argv) > 2 else True
@@ -23,14 +31,14 @@ def main():
     walk = list(os.walk(igrec_dir))
     for root, dirs, files in walk:
         for file in [file for file in files if file.endswith((".py", ".jar", ".sh"))]:
-            dir = os.path.relpath(root, igrec_dir)
-            dest_dir = os.path.join(dest, dir)
-            make_dir_if_not_there(dest_dir)
-            shutil.copy2(os.path.join(root, file), dest_dir)
+            copy_file(dest, file, igrec_dir, root)
+    for file in ["test_dataset/merged_reads.fastq"]:
+        copy_file(dest, os.path.split(file)[-1], igrec_dir, os.path.join(igrec_dir, os.path.dirname(file)))
     for dir in ["build", "configs", "data"]:
         shutil.copytree(os.path.join(igrec_dir, dir), os.path.join(dest, dir))
 
     os.system("git -C %s log | head > %s/GIT_REVISION" % (igrec_dir, dest))
+
 
 if __name__ == '__main__':
     main()
