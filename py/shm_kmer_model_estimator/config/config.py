0f77f7b9f812b26c2cf48bb96e1f31f7d1945174
seryrzu <seryrzu@gmail.com>
2017-02-02 16:43:47 +0300

    (shm model fr cdr) config fixed to start from any directory

--- py/shm_kmer_model_estimator/config/config.py
+++ py/shm_kmer_model_estimator/config/config.py
@@ -2,7 +2,8 @@ import json
 import os.path
 from argparse import Namespace
 
+current_dir = os.path.dirname(os.path.realpath(__file__))
 
-configfile = os.path.join("config", "config.json")
+configfile = os.path.join(current_dir, "config.json")
 with open(configfile, "r") as handle:
     config = json.load(handle, object_hook=lambda d: Namespace(**d))
