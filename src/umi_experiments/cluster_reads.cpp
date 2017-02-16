986cdf0ff5d613dc5985a21b54b8c4e148c654f7
Sergey Bankevich <Sergey.Bankevich@gmail.com>
2017-02-16 17:18:20 +0300

    report umi abundances

--- src/umi_experiments/cluster_reads.cpp
+++ src/umi_experiments/cluster_reads.cpp
@@ -193,6 +193,7 @@ int main(int argc, const char* const* argv) {
 
         clusterer.write_clusters_and_correspondence(params.output_dir, "_5_chimeras", params.save_clusters, params.output_intermediate);
     }
+    clusterer.write_clusters_and_correspondence(params.output_dir, "", params.save_clusters, true, "", "intermediate_repertoire_close_umi.rcm");
 
 //    size_t edit_corrected_reads = clusterer::count_reads_with_corrected_umi(umi_to_clusters_edit_inside_umi, umi_to_clusters_edit_adj_umi);
 //    INFO(edit_corrected_reads << " reads have UMI corrected for edit dist.");
