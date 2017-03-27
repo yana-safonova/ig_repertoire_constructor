09754ee0342fea670b65d9460ada50c80afea044
Andrey Bzikadze <seryrzu@gmail.com>
2017-03-27 20:50:30 +0300

    (igsimulator) multiplicity creator

---
+++ src/ig_simulator/multiplicity_creator.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Andrew Bzikadze on 3/27/17.
+//
+
+#include "multiplicity_creator.hpp"
---
+++ src/ig_simulator/multiplicity_creator.hpp
@@ -0,0 +1,40 @@
+//
+// Created by Andrew Bzikadze on 3/27/17.
+//
+
+#pragma once
+
+#include <cstddef>
+#include "random_generator.hpp"
+#include <random>
+#include "verify.hpp"
+
+namespace ig_simulator {
+
+class AbstractMultiplicityCreator {
+public:
+    virtual size_t RandomMultiplicity() const = 0;
+};
+
+class GeometricMultiplicityCreator : public AbstractMultiplicityCreator {
+private:
+    double lambda;
+    std::exponential_distribution<size_t> distribution;
+
+public:
+    GeometricMultiplicityCreator(double lambda):
+        lambda(lambda),
+        distribution(lambda)
+    {
+        VERIFY(lambda > 0);
+    }
+
+    virtual size_t RandomMultiplicity() const override {
+        return distribution(MTSingleton::GetInstance()) + 1;
+    }
+
+    double Mean() const { return 1. / lambda; }
+
+};
+
+} // End namespace ig_simulator
