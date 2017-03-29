00c7d38ee7827c2a0d4dfa9aec8d6b7221acf392
Andrey Bzikadze <seryrzu@gmail.com>
2017-03-29 16:18:08 +0300

    (igsimulator) multiplicity creator fix

--- src/ig_simulator/multiplicity_creator.hpp
+++ src/ig_simulator/multiplicity_creator.hpp
@@ -13,13 +13,13 @@ namespace ig_simulator {
 
 class AbstractMultiplicityCreator {
 public:
-    virtual size_t RandomMultiplicity() const = 0;
+    virtual size_t RandomMultiplicity() = 0;
 };
 
 class GeometricMultiplicityCreator : public AbstractMultiplicityCreator {
 private:
     double lambda;
-    std::exponential_distribution<size_t> distribution;
+    std::geometric_distribution<size_t> distribution;
 
 public:
     GeometricMultiplicityCreator(double lambda):
@@ -29,7 +29,7 @@ public:
         VERIFY(lambda > 0);
     }
 
-    virtual size_t RandomMultiplicity() const override {
+    virtual size_t RandomMultiplicity() override {
         return distribution(MTSingleton::GetInstance()) + 1;
     }
 
