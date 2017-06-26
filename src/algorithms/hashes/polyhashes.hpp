c5ca0490238261156f4c99cd7da1f2b5b6a7cf6e
Vasiliy Alferov <ya-ikmik2012@yandex.ru>
2017-06-26 23:02:48 +0300

    Apply 4cc8b88..ed5f960

--- src/algorithms/hashes/polyhashes.hpp
+++ src/algorithms/hashes/polyhashes.hpp
@@ -2,6 +2,7 @@
 
 #include <vector>
 #include <seqan/sequence.h>
+#include <seqan/basic.h>
 
 namespace algorithms {
     // TODO Reimplement it as "generator" with O(1) memory consumption
@@ -32,4 +33,30 @@ namespace algorithms {
         }
         return result;
     }
+
+	template<typename T>
+    std::vector<size_t> bitmaskhashes(const T &s, const size_t K) {
+        if (seqan::length(s) < K) {
+            return {  };
+        }
+
+        const size_t mask = (1 << (2 * K)) - 1;
+        const size_t p = 4;
+        std::vector<size_t> result(seqan::length(s) - K + 1);
+
+        size_t first = 0;
+        for (size_t i = 0; i < K; ++i) {
+            first *= p;
+            first += seqan::Dna(s[i]).value;
+        }
+
+        result[0] = first & mask;
+        for (size_t i = 1; i < result.size(); ++i) {
+            first *= p;
+            first += seqan::Dna(s[K + i - 1]).value;
+            result[i] = first & mask;
+        }
+        return result;
+    }
+
 }
