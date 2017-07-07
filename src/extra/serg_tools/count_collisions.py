379c4fa4d53b3f49e35177ed0a30088349d2534b
Sergey Bankevich <Sergey.Bankevich@gmail.com>
2017-07-07 03:54:44 +0300

    add filtering collisions by VJ hits

--- src/extra/serg_tools/count_collisions.py
+++ src/extra/serg_tools/count_collisions.py
@@ -20,11 +20,13 @@ def dist(s, t):
 def main():
     rcm = sys.argv[1]
     cluster_to_umi_with_size = dict()
+    read_to_cluster = dict()
     for line in open(rcm):
-        parts = line.split('\t')
+        parts = line[:-1].split('\t')
         if len(parts) != 2:
             continue
         read_id, cluster_id = parts
+        read_to_cluster[read_id] = cluster_id
         umi = read_id.split('UMI:')[1].split(':')[0]
         if cluster_id not in cluster_to_umi_with_size:
             cluster_to_umi_with_size[cluster_id] = defaultdict(int)
@@ -57,9 +59,34 @@ def main():
         if len(shares) < 2 or shares[-2] < MIN_CLUSTER_SHARE:
             del umi_to_cluster_shares[umi]
 
+
     collisions = [(umi, shares) for umi, shares in umi_to_cluster_shares.iteritems()]
-    print len(collisions)
+    print "collisions: %d" % len(collisions)
     print collisions
+
+
+    vj_hits = get_vj_hits(sys.argv[2])
+    cluster_v_hits = defaultdict(set)
+    cluster_j_hits = defaultdict(set)
+    for read_id, cluster_id in read_to_cluster.iteritems():
+        v_hit, j_hit = vj_hits[read_id]
+        cluster_v_hits[cluster_id].add(v_hit)
+        cluster_j_hits[cluster_id].add(j_hit)
+
+
+    for umi in umi_to_cluster_shares.keys():
+        shares = sorted(umi_to_cluster_shares[umi].values())
+        cluster1 = shares[-1]
+        cluster2 = shares[-2]
+        if not cluster_v_hits[cluster1].isdisjoint(cluster_v_hits[cluster2]):
+            continue
+        if not cluster_j_hits[cluster1].isdisjoint(cluster_j_hits[cluster2]):
+            continue
+        del umi_to_cluster_shares[umi]
+
+    strict_collisions = [(umi, shares) for umi, shares in umi_to_cluster_shares.iteritems()]
+    print "strict collisions: %d" % len(strict_collisions)
+    print strict_collisions
 
 
 if __name__ == '__main__':
