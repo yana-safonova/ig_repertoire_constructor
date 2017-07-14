c74e7c9ded3ca5e23910ac0ddc8fa74f1f3e8a4a
Sergey Bankevich <Sergey.Bankevich@gmail.com>
2017-07-14 16:21:57 +0300

    stats for the paper

--- src/extra/serg_tools/barcoded_igrec/prepare_plots_for_answers.py
+++ src/extra/serg_tools/barcoded_igrec/prepare_plots_for_answers.py
@@ -42,7 +42,22 @@ plt.plot(range(len(data)), data, "b-", color="blue", label="label")
 # plt.ylim(0, 1)
 
 plt.xlabel("barcode tau")
-plt.ylabel("# reads with corrected UMIs")
+plt.ylabel("% reads with corrected UMIs")
 
 save_plot("umi_tau_choice_sim_reads", format="png")
+
+# ------------
+
+initialize_plot()
+
+# data = [152, 1056, 3731, 9474, 14092, 15561, 14491, 12075, 9233, 6642]
+data = [0.3476669716, 2.453075636, 9.200305773, 26.9960677, 45.92322232, 53.09109519, 47.78250404, 37.0330614, 26.21670737, 17.66160555]
+tau = range(0, len(data) * 5, 5)
+plt.plot(tau, data, "b-", color="blue", label="label")
+# plt.ylim(0, 1)
+
+plt.xlabel("chimera tau")
+plt.ylabel("% detected chimeras")
+
+save_plot("chimera_tau_choice", format="png")
 
