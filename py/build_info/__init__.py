8b7f0caadaf27bd8a4b861e94d2085449ec4df50
Alexander Shlemov <shlemovalex@gmail.com>
2017-07-18 19:02:09 +0300

    Fix build_info output

--- py/build_info/__init__.py
+++ py/build_info/__init__.py
@@ -12,10 +12,12 @@ except:
     system = ""
     cpu = ""
 
-    c_compiler_id = ""
-    cxx_compiler_id = ""
     c_compiler = ""
+    c_compiler_id = ""
+    c_compiler_version = ""
     cxx_compiler = ""
+    cxx_compiler_id = ""
+    cxx_compiler_version = ""
 
     build_type = ""
     c_flags = ""
@@ -32,10 +34,14 @@ def Log(log):
     log.info("cmake version: " + cmake)
     log.info("OS version: " + system)
     log.info("CPU: " + cpu)
-    log.info("C compiler id: " + c_compiler_id)
-    log.info("CXX compiler id: " + cxx_compiler_id)
+
     log.info("C compiler: " + c_compiler)
-    log.info("CXX compiler: " + cxx_compiler)
-    log.info("Build type: " + build_type)
+    log.info("C compiler id: " + c_compiler_id)
+    log.info("C compiler version: " + c_compiler_version)
     log.info("C flags: " + c_flags)
-    log.info("CXX flags: " + cxx_flags)
+    log.info("C++ compiler: " + cxx_compiler)
+    log.info("C++ compiler id: " + cxx_compiler_id)
+    log.info("C++ compiler version: " + cxx_compiler_version)
+    log.info("C++ flags: " + cxx_flags)
+
+    log.info("Build type: " + build_type)
--- src/build_info/test_build_info.cpp
+++ src/build_info/test_build_info.cpp
@@ -2,27 +2,32 @@
 
 #include <iostream>
 
+#define OUT_BUILD_INFO_FIELD(field) #field << ": " << build_info::field
+
 int main() {
-    std::cout << build_info::version << std::endl;
+    std::cout << OUT_BUILD_INFO_FIELD(version) << std::endl;
 
-    std::cout << build_info::git_hash << std::endl;
-    std::cout << build_info::git_hash7 << std::endl;
-    std::cout << build_info::git_refspec << std::endl;
-    std::cout << build_info::git_describe << std::endl;
-    std::cout << build_info::git_tag << std::endl;
+    std::cout << OUT_BUILD_INFO_FIELD(git_hash) << std::endl;
+    std::cout << OUT_BUILD_INFO_FIELD(git_hash7) << std::endl;
+    std::cout << OUT_BUILD_INFO_FIELD(git_refspec) << std::endl;
+    std::cout << OUT_BUILD_INFO_FIELD(git_describe) << std::endl;
+    std::cout << OUT_BUILD_INFO_FIELD(git_tag) << std::endl;
 
-    std::cout << build_info::cmake << std::endl;
-    std::cout << build_info::system << std::endl;
-    std::cout << build_info::cpu << std::endl;
+    std::cout << OUT_BUILD_INFO_FIELD(cmake) << std::endl;
+    std::cout << OUT_BUILD_INFO_FIELD(system) << std::endl;
+    std::cout << OUT_BUILD_INFO_FIELD(cpu) << std::endl;
 
-    std::cout << build_info::c_compiler_id << std::endl;
-    std::cout << build_info::c_compiler << std::endl;
-    std::cout << build_info::cxx_compiler_id << std::endl;
-    std::cout << build_info::cxx_compiler << std::endl;
+    std::cout << OUT_BUILD_INFO_FIELD(c_compiler) << std::endl;
+    std::cout << OUT_BUILD_INFO_FIELD(c_compiler_id) << std::endl;
+    std::cout << OUT_BUILD_INFO_FIELD(c_compiler_version) << std::endl;
+    std::cout << OUT_BUILD_INFO_FIELD(c_flags) << std::endl;
 
-    std::cout << build_info::build_type << std::endl;
-    std::cout << build_info::c_flags << std::endl;
-    std::cout << build_info::cxx_flags << std::endl;
+    std::cout << OUT_BUILD_INFO_FIELD(cxx_compiler) << std::endl;
+    std::cout << OUT_BUILD_INFO_FIELD(cxx_compiler_id) << std::endl;
+    std::cout << OUT_BUILD_INFO_FIELD(cxx_compiler_version) << std::endl;
+    std::cout << OUT_BUILD_INFO_FIELD(cxx_flags) << std::endl;
+
+    std::cout << OUT_BUILD_INFO_FIELD(build_type) << std::endl;
 
     return 0;
 }
