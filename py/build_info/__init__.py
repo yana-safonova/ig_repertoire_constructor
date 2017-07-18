e890267e060cab7c76864930453480e6987cd274
Alexander Shlemov <shlemovalex@gmail.com>
2017-07-18 19:21:29 +0300

    build_info: add python version to python version

--- py/build_info/__init__.py
+++ py/build_info/__init__.py
@@ -26,14 +26,14 @@ except:
 
 def Log(log):
     log.info("version: " + version)
-    log.info("git hash: " + git_hash)
+    # log.info("git hash: " + git_hash)
     log.info("git hash7: " + git_hash7)
-    log.info("git refspec: " + git_refspec)
+    # log.info("git refspec: " + git_refspec)
     log.info("git describe: " + git_describe)
-    log.info("git tag: " + git_tag)
+    # log.info("git tag: " + git_tag)
     log.info("cmake version: " + cmake)
-    log.info("OS version: " + system)
-    log.info("CPU: " + cpu)
+    log.info("Platform: " + system)
+    log.info("Architecture: " + cpu)
 
     log.info("C compiler: " + c_compiler)
     log.info("C compiler id: " + c_compiler_id)
@@ -45,3 +45,28 @@ def Log(log):
     log.info("C++ flags: " + cxx_flags)
 
     log.info("Build type: " + build_type)
+
+    import sys
+    log.info("Python version: " + sys.version.replace("\n", "\t"))
+
+
+    def package_version(package):
+        import importlib
+        try:
+            return importlib.import_module(package).__version__
+        except:
+            return "<failed to import, try reinstalling>"
+
+
+    def check_package(package):
+        import importlib
+        try:
+            importlib.import_module(package)
+            return True
+        except:
+            return False
+
+
+    log.info("Packages versions:")
+    for package in ["matplotlib", "seaborn", "numpy", "scipy", "Bio", "pandas"]:
+        log.info("\t%s: %s" % (package, package_version(package)))
