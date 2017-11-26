50bb3803c68bafba1aff0e9663c4e96c9e21dc33
Alexander Shlemov <shlemovalex@gmail.com>
2017-11-27 01:56:41 +0300

    Fix docker runner script

--- web/docker/run.sh
+++ web/docker/run.sh
@@ -8,3 +8,7 @@ docker run -p 15284:8000 --mount type=bind,source=/Bmo/ashlemov/igrec-web/runs,d
     --mount type=bind,source=/,destination=/host-root,readonly \
     igrec-web:latest
 
+# Use
+# ssh -C -q -N -o ServerAliveInterval=60 ashlemov@chihua.cab.spbu.ru -L 8000:localhost:15284
+# To connect to server
+
