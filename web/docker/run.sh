c8aa65956ee1598c272da90629c8dde3e3709d5d
Alexander Shlemov <shlemovalex@gmail.com>
2017-11-30 23:41:31 +0300

    Web fix runner script

--- web/docker/run.sh
+++ web/docker/run.sh
@@ -1,14 +1,22 @@
+#!/bin/bash
+
+set -euxo pipefail
+
 docker build . -t igrec-web
 
-mkdir -p /Bmo/ashlemov/igrec-web/runs
-mkdir -p /Bmo/ashlemov/igrec-web/uploads
+RUN_DIR="/Bmo/ashlemov/igrec-web/runs"
+UPLOAD_DIR="/Bmo/ashlemov/igrec-web/uploads"
+mkdir -p "${RUN_DIR}"
+mkdir -p "${UPLOAD_DIR}"
 
-docker run -p 15284:8000 --mount type=bind,source=/Bmo/ashlemov/igrec-web/runs,destination=/opt/y-tools/web/runs \
-    --mount type=bind,source=/Bmo/ashlemov/igrec-web/uploads,destination=/opt/y-tools/web/tmp \
+docker run -p 15284:8000 \
+    --mount type=bind,source="${RUN_DIR}",destination=/opt/y-tools/web/runs \
+    --mount type=bind,source="${UPLOAD_DIR}",destination=/opt/y-tools/web/tmp \
+    --mount type=bind,source="${UPLOAD_DIR}",destination=/uploads \
     --mount type=bind,source=/,destination=/host-root,readonly \
     igrec-web:latest
 
 # Use
-# ssh -C -q -N -o ServerAliveInterval=60 ashlemov@chihua.cab.spbu.ru -L 8000:localhost:15284
+# ssh -C -q -N -o ServerAliveInterval=60 <USERNAME>@chihua.cab.spbu.ru -L 8000:localhost:15284
 # To connect to server
 
