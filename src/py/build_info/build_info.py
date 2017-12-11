3117ebd47a0b0683c0d185f0c13875d154c59749
Maria Chernigovskaya <mchernigovskaya@chihua.cab.spbu.ru>
2017-12-11 13:44:47 +0300

    Fit v segments length to croped germline

---
+++ src/py/build_info/build_info.py
@@ -0,0 +1,22 @@
+version = "<unknown>"
+git_hash = "092d3a76bf1ddfb586af78ba610a91d65a92056e"
+git_hash7 = "092d3a7"
+git_refspec = "refs/heads/antevolo_update"
+git_describe = ""
+git_tag = ""
+
+cmake = "3.8.2"
+system = "Linux-4.13.0-17-generic"
+cpu = "x86_64"
+
+c_compiler = "/usr/bin/x86_64-linux-gnu-gcc-7"
+c_compiler_id = "GNU"
+c_compiler_version = "7.2.0"
+
+cxx_compiler = "/usr/bin/x86_64-linux-gnu-g++-7"
+cxx_compiler_id = "GNU"
+cxx_compiler_version = "7.2.0"
+
+build_type = "Debug"
+c_flags = " -fopenmp"
+cxx_flags = " -fopenmp -std=c++0x -Wno-deprecated"
